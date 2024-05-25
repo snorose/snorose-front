e2f89922319daa9e47be94b693f9358211dcd68a
abi-hong <flffjakfwm20@gmail.com>
2024-05-25 21:37:16 +0900

    feat(board): list 컴포넌트 데이터 정의 및 연결

--- shared/components/atom/list/list.component.ts
+++ shared/components/atom/list/list.component.ts
@@ -1,11 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { IBoardListData } from '../../../http/board.http';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
+
   @Input() lists: IBoardListData[] = [];
+
+  public name: string | null = null;
+  public boardId: string | null = null;
+
+  constructor(
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.name = this.route.snapshot.paramMap.get('name');
+    this.boardId = this.route.snapshot.paramMap.get('boardId');
+  }
+
 }
