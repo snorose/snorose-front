070868decadb6727989667e738caa40d5896eba0
abi-hong <flffjakfwm20@gmail.com>
2024-05-25 15:44:59 +0900

    feat(dal): snackbar 생성

--- shared/services/dal.service.ts
+++ shared/services/dal.service.ts
@@ -2,18 +2,32 @@ import { Injectable } from '@angular/core';
 import { ReviewHttp } from '../http/review.http';
 import { HttpService } from './http.service';
 import { MembershipHttp } from '../http/membership.http';
+import { BoardHttp } from '../http/board.http';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DalService {
 
+  public boardHttp: BoardHttp;
   public reviewHttp: ReviewHttp;
   public membershipHttp: MembershipHttp;
 
-  constructor(private httpService: HttpService) {
-    this.reviewHttp = new ReviewHttp(httpService);
-    this.membershipHttp = new MembershipHttp(httpService);
+  constructor(
+    private _snackBar: MatSnackBar
+  ) {
+    this.boardHttp = new BoardHttp();
+    this.reviewHttp = new ReviewHttp();
+    this.membershipHttp = new MembershipHttp();
+  }
+
+  public snackBar(text: string, undo: string = "", duration: number = 2000) {
+    this._snackBar.open(text, undo, {
+      duration: duration,
+      horizontalPosition: "center",
+      verticalPosition: "bottom",
+    });
   }
 
 }
