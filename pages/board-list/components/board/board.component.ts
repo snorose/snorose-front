eb0d0917a50584e58e41103f6b9fa13007e39ec7
abi-hong <flffjakfwm20@gmail.com>
2024-05-25 21:36:07 +0900

    fix(board): null 처리

--- pages/board-list/components/board/board.component.ts
+++ pages/board-list/components/board/board.component.ts
@@ -9,8 +9,9 @@ import { IBoardListData } from '../../../../shared/http/board.http';
   styleUrl: './board.component.scss'
 })
 export class BoardComponent implements OnInit {
-  public name!: string | null;
-  public boardId!: string | null;
+
+  public name: string | null = null;
+  public boardId: string | null = null;
   public boardList: IBoardListData[] = [];
 
   constructor(
@@ -27,7 +28,7 @@ export class BoardComponent implements OnInit {
       this.dalService.snackBar('board Id가 없습니다.');
     }
     this.dalService.boardHttp.getBoardList(this.boardId as string, 0).subscribe(response => {
-      console.log('getBoardList response', response);
+      // console.log('getBoardList response', response);
       this.boardList = response.result;
     });
   }
