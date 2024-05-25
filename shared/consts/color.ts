c9335f9d25642a6ad8fc732aeb53695ec5a36ea3
abi-hong <flffjakfwm20@gmail.com>
2024-05-25 23:10:17 +0900

    feat(color): color 상수화

---
+++ shared/consts/color.ts
@@ -0,0 +1,11 @@
+export const BLUE1 = '#00368E';
+export const BLUE2 = '#5F86BF';
+export const BLUE3 = '#BFD7EC';
+export const BLUE4 = '#DDEBF6';
+
+export const GRAY1 = '#C7C7C7';
+export const GRAY2 = '#F0F0F0';
+export const GRAY3 = '#F8F8F8';
+
+export const PINK1 = '#FF0000';
+export const PINK2 = '#FFE8E8';
