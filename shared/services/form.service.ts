4ecda62ff2a3789de4b5af0f515e97c9a03ee495
abi-hong <flffjakfwm20@gmail.com>
2024-06-01 01:42:05 +0900

    feat(form): form 서비스 생성 및 개발

---
+++ shared/services/form.service.ts
@@ -0,0 +1,51 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpService } from './http.service';
+
+export interface IValidation {
+  name: string;
+  validator: string;
+  message: string;
+}
+
+export interface IGroup {
+  main: string;
+  sub: string[];
+}
+
+export interface IStyle {
+  layout?: any;
+  label?: any;
+  input?: any;
+  textarea?: any;
+  password?: any;
+  radioButton?: any;
+}
+
+export interface IControl {
+  category: string;
+  type: string;
+  label?: string;
+  name: string;
+  value?: any;
+  group?: IGroup[]; // select type을 위한 group
+  placeholder?: string;
+  validations?: IValidation[],
+  styles?: IStyle;
+}
+
+export interface IFormGroup extends IControl {
+  controls: IControl[];
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FormService {
+
+  private readonly httpService = inject(HttpService);
+
+  public getFormStructure() {
+    return this.httpService.Get('./assets/json/form.json');
+  }
+
+}
