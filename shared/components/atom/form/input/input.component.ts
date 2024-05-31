b07d204ad14a8da48bd253627894ad10b092ce1c
abi-hong <flffjakfwm20@gmail.com>
2024-06-01 01:41:04 +0900

    feat(form): input 컴포넌트 생성 및 개발

---
+++ shared/components/atom/form/input/input.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { FormAbstract } from '../form.abstract';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.scss'
+})
+export class InputComponent extends FormAbstract {
+
+}
