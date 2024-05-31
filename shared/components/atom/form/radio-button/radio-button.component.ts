f7629c23145a30b8884a844ad646f69d03d7c471
abi-hong <flffjakfwm20@gmail.com>
2024-06-01 01:40:28 +0900

    feat(form): radio-button 생성 및 개발

---
+++ shared/components/atom/form/radio-button/radio-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { FormAbstract } from '../form.abstract';
+
+@Component({
+  selector: 'app-radio-button',
+  templateUrl: './radio-button.component.html',
+  styleUrl: './radio-button.component.scss'
+})
+export class RadioButtonComponent extends FormAbstract {
+
+}
