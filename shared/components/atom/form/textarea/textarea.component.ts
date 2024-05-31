186784508ea9a721e955e3d2158df9aac631010a
abi-hong <flffjakfwm20@gmail.com>
2024-06-01 01:39:48 +0900

    feat(form): textarea 컴포넌트 생성 및 개발

---
+++ shared/components/atom/form/textarea/textarea.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import { FormAbstract } from '../form.abstract';
+
+@Component({
+  selector: 'app-textarea',
+  templateUrl: './textarea.component.html',
+  styleUrl: './textarea.component.scss'
+})
+export class TextareaComponent extends FormAbstract {
+
+}
