04ccf2bec203073d055dc077c2c781f78b02eb97
abi-hong <flffjakfwm20@gmail.com>
2024-06-02 19:53:34 +0900

    feat(sign-up): form 데이터로 동적 폼 만들기

--- pages/membership/components/sign-up/sign-up.component.ts
+++ pages/membership/components/sign-up/sign-up.component.ts
@@ -26,25 +26,10 @@ export class SignUpComponent {
   public majorList = majorList;
   public BLUE1 = BLUE1;
 
-  public firstFormGroup = this.formBuilder.group({
-    userName: ['', Validators.required],
-    loginId: ['', Validators.required],
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', Validators.required],
-    checkedPassword: ['', Validators.required]
-  });
-
-  public secondFormGroup = this.formBuilder.group({
-    nickname: ['', Validators.required],
-    major: ['', Validators.required],
-    studentNumber: [0, Validators.required],
-    birthday: [new Date(), Validators.required],
-  });
 
 
   constructor(
     private router: Router,
-    private formBuilder: FormBuilder,
     private readonly dalService: DalService,
     private readonly dateService: DateService,
     public readonly layoutService: LayoutService,
@@ -52,32 +37,7 @@ export class SignUpComponent {
 
 
   public onSignUpClick(event: any) {
-    console.log('firstFormGroup', this.firstFormGroup.value, 'secondFormGroup', this.secondFormGroup.value);
-
-    if (!this.firstFormGroup.valid) {
-      this.dalService.snackBar('첫번째 양식에 입력하지 않은 필드가 있습니다. 모두 입력 후, 제출해 주세요');
-      return;
-    }
-    if (!this.secondFormGroup.valid) {
-      this.dalService.snackBar('두번째 양식에 입력하지 않은 필드가 있습니다. 모두 입력 후, 제출해 주세요');
-      return;
-    }
-
-    const signUpData: ISignUpRequest = {
-      loginId: this.firstFormGroup.value.loginId as string,
-      password: this.firstFormGroup.value.password as string,
-      checkedPassword: this.firstFormGroup.value.checkedPassword as string,
-      userName: this.firstFormGroup.value.userName as string,
-      email: this.firstFormGroup.value.email as string,
-      nickname: this.secondFormGroup.value.nickname as string,
-      studentNumber: this.secondFormGroup.value.studentNumber as number,
-      major: this.secondFormGroup.value.major as string,
-      birthday: this.dateService.getBirthdayString(this.secondFormGroup.value.birthday as Date)
-    };
-
-    console.log('signUpData', signUpData);
-
-    this.dalService.membershipHttp.signUp(signUpData).subscribe((response: ISignUpResponse) => {
+    this.dalService.membershipHttp.signUp(event.value).subscribe((response: ISignUpResponse) => {
       console.log('response^^', response);
       if (response.isSuccess) {
         this.dalService.snackBar(`회원가입을 성공했습니다. 로그인 해주세요.`);
