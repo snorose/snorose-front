439858466972059fe69856c462284442fc4d1e4c
abi-hong <flffjakfwm20@gmail.com>
2024-06-06 18:34:26 +0900

    feat(comment): 댓글 http 클래스 생성

---
+++ shared/http/comment.http.ts
@@ -0,0 +1,65 @@
+import { Observable } from "rxjs";
+import { IBaseResponse } from "./baseResponse.dto";
+import { inject } from "@angular/core";
+import { HttpService } from "../services/http.service";
+
+export interface ICommentData {
+  id: number;
+  postId: number;
+  userProfile: string | null;
+  userDisplay: string;
+  content: string;
+  likeCount: number;
+  reportCount: number;
+  createdAt: string;
+  updatedAt: string | null;
+  deletedAt: string | null;
+  isVisible: boolean;
+  isUpdated: boolean;
+  isDeleted: boolean;
+  children: ICommentData[]
+}
+
+export interface ICommentCreateRequest {
+  userDisplay: string;
+  parentId: number | null;
+  content: string;
+}
+
+export interface ICommentUpdateRequest {
+  content: string;
+}
+
+export interface ICommentListResponse extends IBaseResponse<Array<ICommentData>>{}
+export interface ICommentCreateResponse extends IBaseResponse<ICommentData>{}
+export interface ICommentUpdateResponse extends IBaseResponse<ICommentData>{}
+export interface ICommentDeleteResponse extends IBaseResponse<ICommentData>{}
+
+export interface ICommentHttp {
+  getList(postId: string): Observable<ICommentListResponse>;
+  create(postId: string, request: ICommentCreateRequest): Observable<ICommentCreateResponse>;
+  update(postId: string, commentId: string, request: ICommentUpdateRequest): Observable<ICommentUpdateResponse>;
+  delete(postId: string, commentId: string): Observable<ICommentDeleteResponse>;
+}
+
+export class CommentHttp implements ICommentHttp {
+
+  private readonly httpService = inject(HttpService);
+
+  getList(postId: string): Observable<ICommentListResponse> {
+    return this.httpService.Get(`/v1/posts/${postId}/comments`);
+  }
+  
+  create(postId: string, request: ICommentCreateRequest): Observable<ICommentCreateResponse> {
+    return this.httpService.Post(`/v1/posts/${postId}/comments`, request);
+  }
+  
+  update(postId: string, commentId: string, request: ICommentUpdateRequest): Observable<ICommentUpdateResponse> {
+    return this.httpService.Patch(`/v1/posts/${postId}/comments/${commentId}`, request);
+  }
+
+  delete(postId: string, commentId: string): Observable<ICommentDeleteResponse> {
+    return this.httpService.Delete(`/v1/posts/${postId}/comments/${commentId}`);
+  }
+
+}
--- shared/services/dal.service.ts
+++ shared/services/dal.service.ts
@@ -5,6 +5,7 @@ import { MembershipHttp } from '../http/membership.http';
 import { BoardHttp } from '../http/board.http';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { PointHttp } from '../http/point.http';
+import { CommentHttp } from '../http/comment.http';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +16,7 @@ export class DalService {
   public reviewHttp: ReviewHttp;
   public membershipHttp: MembershipHttp;
   public pointHttp: PointHttp;
+  public commentHttp: CommentHttp;
 
   constructor(
     private _snackBar: MatSnackBar
@@ -23,6 +25,7 @@ export class DalService {
     this.reviewHttp = new ReviewHttp();
     this.membershipHttp = new MembershipHttp();
     this.pointHttp = new PointHttp();
+    this.commentHttp = new CommentHttp();
   }
 
   public snackBar(text: string, undo: string = "", duration: number = 2000) {
