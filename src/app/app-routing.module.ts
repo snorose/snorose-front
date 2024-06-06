8e5538dfcc6c3fa8da8a4b4a056aca59ae97e9ca
abi-hong <flffjakfwm20@gmail.com>
2024-06-06 18:31:48 +0900

    refactor(router): 게시판 및 시험후기 라우터 변경

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from '../../pages/membership/components/sign-in/sign-in.component';
-import { SignUpComponent } from '../../pages/membership/components/sign-up/sign-up.component';
 import { PageNotFoundComponent } from '../../pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
@@ -15,20 +14,16 @@ const routes: Routes = [
     loadChildren: () => import('../../pages/main/main.module').then(m => m.MainModule),
   },
   {
-    path: 'file',
-    loadChildren: () => import('../../pages/file/file.module').then(m => m.FileModule),
-  },
-  {
-    path: 'board-list',
-    loadChildren: () => import('../../pages/board-list/board-list.module').then(m => m.BoardListModule),
-  },
-  {
-    path: 'post',
-    loadChildren: () => import('../../pages/post/post.module').then(m => m.PostModule),
+    path: 'board',
+    loadChildren: () => import('../../pages/board/board.module').then(m => m.BoardModule),
   },
   {
     path: 'membership',
     loadChildren: () => import('../../pages/membership/membership.module').then(m => m.MembershipModule),
+  },
+  {
+    path: 'review',
+    loadChildren: () => import('../../pages/review/review.module').then(m => m.ReviewModule),
   },
   {
     path: 'signIn',
