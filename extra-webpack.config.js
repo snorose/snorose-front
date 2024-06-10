f5ec99363b13286939d383a8557e5a8225fed6a4
Heesoo Hong <68041042+abi-hong@users.noreply.github.com>
2024-06-10 18:19:32 +0900

    chore(env): env 설정 변경

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -2,9 +2,6 @@ const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   plugins: [
-    new Dotenv({
-      path: './.env', // 환경 변수 파일의 경로
-      safe: true // '.env.example' 파일과 함께 사용하여 키가 모두 설정되어 있는지 확인합니다.
-    })
+    new Dotenv()
   ]
 };
