601375c80c29a644da7c17fb52d549195de589b6
Heesoo Hong <68041042+abi-hong@users.noreply.github.com>
2024-06-11 18:15:46 +0900

    fix(navigation): bottom 네비게이션 클릭 시, 색 변하도록 수정

--- shared/components/layouts/bottom-navigation/bottom-navigation.component.ts
+++ shared/components/layouts/bottom-navigation/bottom-navigation.component.ts
@@ -1,14 +1,45 @@
-import { Component } from '@angular/core';
-import { BLUE2, BLUE3 } from '../../../consts/color';
+import { Component, OnInit, inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+interface INavigation {
+  name: string;
+  icon: string;
+  url: string;
+  select: boolean;
+}
 
 @Component({
   selector: 'app-bottom-navigation',
   templateUrl: './bottom-navigation.component.html',
   styleUrl: './bottom-navigation.component.scss'
 })
-export class BottomNavigationComponent {
+export class BottomNavigationComponent implements OnInit {
 
-  public BLUE2 = BLUE2;
-  public BLUE3 = BLUE3;
+  private readonly router = inject(Router);
+
+  public navigations: INavigation[] = [
+    { name: '메인홈', icon: 'home', url: '/main', select: false },
+    { name: '게시판', icon: 'lists', url: '/board', select: false },
+    { name: '시험후기', icon: 'local_library', url: '/review', select: false },
+    { name: '마이페이지', icon: 'face_6', url: '/membership/myPage', select: false }
+  ]
+  public currentNavigation: INavigation = this.navigations.first();
+
+  ngOnInit() {
+    const currentUrl = this.router.url;
+
+    this.navigations.forEach(navigation => {
+      if (navigation.url === currentUrl) {
+        navigation.select = true;
+        this.currentNavigation = navigation;
+      }
+    });
+  }
+
+  navigationClick(navigation: INavigation) {
+    this.navigations.forEach(nav => nav.select = false);
+    navigation.select = true;
+    this.router.navigateByUrl(navigation.url);
+  }
 
 }
