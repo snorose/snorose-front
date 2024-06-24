a820c9abc19057dfaec8b11a64b425cb914fe157
abi-hong <flffjakfwm20@gmail.com>
2024-06-24 23:04:44 +0900

    feat(membership): 아이디 찾기 서버 연결

--- pages/membership/components/find/id/id.component.ts
+++ pages/membership/components/find/id/id.component.ts
@@ -12,8 +12,25 @@ export class IdComponent {
   private readonly dalService = inject(DalService);
   private readonly location = inject(Location);
 
+  public isFindId: boolean = false;
+  public loginId: string | null = null;
+
   public onSubmit(event: any) {
     console.log('onSubmit', event);
+    this.dalService.membershipHttp.findId(event.value).subscribe({
+      next: (response) => {
+        if (response.isSuccess) {
+          this.loginId = response.result.loginId;
+          this.isFindId = true;
+        }
+        else {
+          this.dalService.snackBar(response.message);
+        }
+      },
+      error: (error) => {
+        this.dalService.snackBar('서버와의 통신 중 오류가 발생했습니다. 다시 시도해주세요');
+      }
+    });
   }
 
   public goBack(): void {
