01212670f6a884687864571a7a1be4395105d452
abi-hong <flffjakfwm20@gmail.com>
2024-06-24 23:08:21 +0900

    feat(dialog): 다이얼로그 컴포넌트 생성

--- shared/components/atom/confirm-dialog/confirm-dialog.component.ts
+++ shared/components/atom/confirm-dialog/confirm-dialog.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+export interface IConfirmDialog {
+  title: string;
+  content: string;
+  btnYes: string;
+  btnNo: string;
+  isBtnDisabled: boolean;
+  isDisableClose: boolean;
+}
 
 @Component({
   selector: 'app-confirm-dialog',
@@ -6,5 +16,31 @@ import { Component } from '@angular/core';
   styleUrl: './confirm-dialog.component.scss'
 })
 export class ConfirmDialogComponent {
+
+  public title: string;
+  public content: string;
+  public btnYes: string;
+  public btnNo: string;
+  public isBtnDisabled: boolean;
+
+  constructor(
+    private dialogRef: MatDialogRef<IConfirmDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: IConfirmDialog
+  ) {
+    this.title = data.title;
+    this.content = data.content;
+    this.btnYes = data.btnYes;
+    this.btnNo = data.btnNo;
+    this.isBtnDisabled = data.isBtnDisabled ?? false;
+    this.dialogRef.disableClose = data.isDisableClose ?? false;
+  }
+
+  public onClickNo() {
+    this.dialogRef.close(false);
+  }
+
+  public onClickYes() {
+    this.dialogRef.close(true);
+  }
 
 }
