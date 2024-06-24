e87f119b0eaa74b89046656be7def6f06aff43c6
abi-hong <flffjakfwm20@gmail.com>
2024-06-24 23:09:07 +0900

    fix(point): 포인트 enum 정의

--- shared/data/point.data.ts
+++ shared/data/point.data.ts
@@ -21,14 +21,27 @@ export enum POINT_SOURCE {
 11. 이벤트
 */
 export enum POINT_CATEGORY {
-  Attendance = '출석체크',
-  Post_Write = '게시글 작성',
-  Post_Delete = '게시글 삭제',
-  Comment_Write = '댓글 작성',
-  Comment_Delete = '댓글 삭제',
-  Review_Write = '시험후기 작성',
-  Review_Download = '시험후기 다운로드',
-  Plus = '보상',
-  Minus = '차감',
-  Event = '이벤트'
+  Attendance = 'ATTENDANCE',
+  Post_Create = 'POST_CREATE',
+  Post_Delete = 'POST_DELETE',
+  Comment_Create = 'COMMENT_CREATE',
+  Comment_Delete = 'COMMENT_DELETE',
+  Exam_Review_Create = 'EXAM_REVIEW_CREATE',
+  Exam_Review_Download = 'EXAM_REVIEW_DOWNLOAD',
+  Exam_Review_Delete = 'EXAM_REVIEW_DELETE',
+  Lecture_Review_Create = 'LECTURE_REVIEW_CREATE',
+  Lecture_Review_Delete = 'LECTURE_REVIEW_DELETE',
+  Point_Reward_Report_General = 'POINT_REWARD_REPORT_GENERAL',
+  Point_Reward_Report_Exam_Review = 'POINT_REWARD_REPORT_EXAM_REVIEW',
+  Point_Reward_Report_Outsider = 'POINT_REWARD_REPORT_OUTSIDER',
+  Point_Reward_Report_Permanent_Demotion = 'POINT_REWARD_REPORT_PERMANENT_DEMOTION',
+  Point_Reward_10_Likes = 'POINT_REWARD_10_LIKES',
+  Point_Reward_100_Likes = 'POINT_REWARD_100_LIKES',
+  Point_Reward_1000_Likes = 'POINT_REWARD_1000_LIKES',
+  Point_Reward_User_Auth = 'POINT_REWARD_USER_AUTH',
+  Point_Reward_Etc = 'POINT_REWARD_ETC',
+  Point_Deduction_Flooding = 'POINT_DEDUCTION_FLOODING',
+  Point_Deduction_Etc = 'POINT_DEDUCTION_ETC',
+  Event = 'EVENT',
+  Etc = 'ETC'
 }
