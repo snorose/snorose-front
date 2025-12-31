b507fc0464354530d88c17f66d2a535932211ed5
misung.dev <misung.dev@gmail.com>
2025-12-31 17:58:03 +0900

    #134 [FEAT] 팝업 디자인 본문 수정

--- src/feature/home/component/PopUp/PopUp.jsx
+++ src/feature/home/component/PopUp/PopUp.jsx
@@ -4,34 +4,58 @@ import { usePopUp } from '@/feature/home/hook';
 import styles from './PopUp.module.css';
 import calendar from './calendar.png';
 
+const POPUP_CONTENTS = [
+  {
+    title: '1월 스노로즈 일정',
+    content: null,
+    image: calendar,
+  },
+  {
+    title: null,
+    content: [
+      '스노로즈 인스타그램(@snorose1906)에서 월별 스노로즈 일정을 쉽게 확인할 수 있습니다.',
+      '공식 문의 창구 [이메일(snorose1906@gmail.com), 카카오톡 1:1 문의] 이외의 문의는 받고 있지 않습니다. 공식 문의 창구 이외의 문의 글은 답변 없이 삭제될 수 있음을 알려드립니다.',
+    ],
+    image: null,
+  },
+];
+
 const content = (
   <div className={styles.notice}>
     <div>
-      <p className={styles.hello}>
-        ❄️ 안녕하세요. 숙명인을 위한 커뮤니티, 스노로즈입니다. ❄️
+      <span className={styles.popupTitle}>스노로즈 공지</span>
+    </div>
+    <div>
+      <p className={styles.popupContent}>
+        안녕하세요!
+        <br />
+        숙명인을 위한 커뮤니티, 스노로즈입니다.
       </p>
-
-      <Heading>1. 1월 캘린더</Heading>
-      <Content>
-        <img src={calendar} alt='1월 캘린더' />
-      </Content>
-
-      <hr />
-      <Content>
-        ※ 공식 문의 창구 (
-        <span>
-          이메일(
-          <a
-            href='mailto:snorose1906@gmail.com'
-            style={{ color: '#007bff', textDecoration: 'underline' }}
-          >
-            snorose1906@gmail.com
-          </a>
-          )
-        </span>
-        , 카카오톡 1:1 문의) 이외의 문의는 받고 있지 않습니다. 공식 문의 창구
-        이외의 문의 글은 답변 없이 삭제될 수 있음을 알려드립니다.
-      </Content>
+    </div>
+    <div className={styles.popupSectionContainer}>
+      {POPUP_CONTENTS.map((section) => (
+        <div key={section.title} className={styles.popupSection}>
+          {section.title && (
+            <span className={styles.popupSectionTitle}>{section.title}</span>
+          )}
+          {section.content && (
+            <ul>
+              {section.content.map((content) => (
+                <li key={content} className={styles.popupSectionContent}>
+                  {content}
+                </li>
+              ))}
+            </ul>
+          )}
+          {section.image && (
+            <img
+              src={section.image}
+              alt={section.title}
+              className={styles.popupSectionImage}
+            />
+          )}
+        </div>
+      ))}
     </div>
   </div>
 );
