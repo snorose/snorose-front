e8114f03bbcea3874834369ca16e848ba8332988
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2026-01-08 03:51:07 +0900

    #1286 [FIX] changeImage/VideoUpload를 hook으로 분리

--- src/feature/board/component/AttachmentBar/AttachmentBar.jsx
+++ src/feature/board/component/AttachmentBar/AttachmentBar.jsx
@@ -1,102 +1,23 @@
 import { React, useRef, useState } from 'react';
 
 import { Icon } from '@/shared/component';
-import { useToast } from '@/shared/hook';
-import {
-  ATTACHMENT_SIZE_LIMIT,
-  ATTACHMENT_EXTENSION_LIMIT,
-} from '@/shared/constant';
-import {
-  checkImageQuantity,
-  checkImageSize,
-  checkIfImage,
-  checkVideoQuantity,
-  checkVideoSize,
-  checkIfVideo,
-} from '@/feature/attachment/lib';
+import { ATTACHMENT_EXTENSION_LIMIT } from '@/shared/constant';
+import { useAttachmentUpload } from '@/feature/attachment/hook';
 
 import styles from './AttachmentBar.module.css';
 
 export default function AttachmentBar({ attachmentsInfo, setAttachmentsInfo }) {
-  const { toast } = useToast();
   const img = useRef();
   const vid = useRef();
+
+  const { changeImageUpload, changeVideoUpload } = useAttachmentUpload({
+    attachmentsInfo,
+    setAttachmentsInfo,
+  });
 
   //이미지*영상 첨부 버튼의 UI 상태를 좌우함
   const [isImageIconHighlighted, setIsImageIconHighlighted] = useState(false);
   const [isVideoIconHighlighted, setIsVideoIconHighlighted] = useState(false);
-
-  const changeImageUpload = async (e) => {
-    const newFiles = e.target.files;
-    const newFileArray = Array.from(newFiles).filter(
-      (file) => file.size <= ATTACHMENT_SIZE_LIMIT.imageFileSize
-    );
-
-    try {
-      //이미지 확장자 확인
-      checkIfImage(newFiles);
-      //이미지 첨부 개수 제한
-      checkImageQuantity(attachmentsInfo, newFiles);
-      //이미지 용량 제한
-      const warningMessage = checkImageSize(newFiles);
-      if (warningMessage) {
-        toast({
-          message: warningMessage,
-          variant: 'info',
-        });
-      }
-    } catch (e) {
-      toast({ message: e.message, variant: 'info' });
-      return;
-    }
-
-    //새로 선택한 이미지들을 attachmentsInfo 리스트 맨 뒤에 추가해주기
-    const mappedFiles = newFileArray.map((file) => ({
-      fileName: file.name,
-      fileComment: '',
-      fileType: file.type,
-      type: 'PHOTO',
-      file: file,
-      id: '',
-    }));
-    setAttachmentsInfo((prev) => [...prev, ...mappedFiles]);
-
-    //같은 파일 선택할 시 함수가 트리거되지 않는 것을 방지함
-    e.target.value = null;
-  };
-
-  const changeVideoUpload = async (e) => {
-    const newFiles = e.target.files;
-    const newFileArray = Array.from(newFiles).filter(
-      (file) => file.size <= ATTACHMENT_SIZE_LIMIT.videoFileSize
-    );
-
-    try {
-      //영상 확장자 확인
-      checkIfVideo(newFiles);
-      //영상 첨부 개수 제한
-      checkVideoQuantity(attachmentsInfo, newFiles);
-      //영상 용량 제한
-      checkVideoSize(newFiles);
-    } catch (e) {
-      toast({ message: e.message, variant: 'info' });
-      return;
-    }
-
-    //새로 선택한 영상들을 attachmentsInfo 리스트 맨 뒤에 추가해주기
-    const mappedFiles = newFileArray.map((file) => ({
-      fileName: file.name,
-      fileComment: '',
-      fileType: file.type,
-      type: 'VIDEO',
-      file: file,
-      id: '',
-    }));
-    setAttachmentsInfo((prev) => [...prev, ...mappedFiles]);
-
-    //같은 파일 선택할 시 함수가 트리거되지 않는 것을 방지함
-    e.target.value = null;
-  };
 
   return (
     <div className={styles.bar}>
