6f33940e2ab4b25a2950f8095868795b01be450a
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2026-01-13 02:17:25 +0900

    #1338 [FIX] FAQ 문구 변경

--- src/page/support/FAQPage/FAQPage.jsx
+++ src/page/support/FAQPage/FAQPage.jsx
@@ -5,7 +5,7 @@ import { Accordion } from '@/feature/home/component';
 import style from './FAQPage.module.css';
 import { BackAppBar } from '@/shared/component';
 import { renderTextWithLinks } from '@/shared/lib/hyperlink';
-import { faqList } from '../../../feature/support/constant/faqList';
+import { faqList } from '@/feature/support/constant/faqList';
 
 export default function FAQPage() {
   const [openIndex, setOpenIndex] = useState(null);
@@ -19,9 +19,9 @@ export default function FAQPage() {
       <BackAppBar title='자주 묻는 질문' notFixed />
 
       <div className={style.noticeContainer}>
-        {`자주 묻는 질문을 확인해보세요.\n궁금하신 점이 해결되지 않았다면, 아래 공식 창구로 문의해주세요.`}
+        {`자주 묻는 질문을 확인해 보세요.\n궁금하신 점이 해결되지 않았다면, 아래 공식 창구로 문의해 주세요.`}
         <div>
-          <p>[공식창구]</p>
+          <p>[공식 문의 창구]</p>
           <p>- 이메일: snorose1906@gmail.com</p>
           <p>- 카카오톡: 1:1 문의</p>
         </div>
