e0ed52fbd51b52ffc5d444f0897d0a0e11ee9fab
haesa <xdhaesa@gmail.com>
2026-02-02 18:14:04 +0900

    [MODIFY] #1388 로그인 네비게이션 UI를 alert에서 모달로 변경

--- src/RequireAuth.tsx
+++ src/RequireAuth.tsx
@@ -1,6 +1,7 @@
-import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 
 import { useAuth } from '@/shared/hook';
+import { NoticeModal } from '@/shared/component';
 import { USER_STATUS } from '@/shared/constant';
 
 export default function RequireAuth({
@@ -8,6 +9,7 @@ export default function RequireAuth({
 }: {
   children: React.ReactNode;
 }) {
+  const navigate = useNavigate();
   const location = useLocation();
   const { status } = useAuth();
 
@@ -16,9 +18,20 @@ export default function RequireAuth({
   }
 
   if (status === USER_STATUS.isLogout) {
-    alert('로그인이 필요합니다.');
+    const modalText = {
+      title: '로그인이 필요해요',
+      description: null,
+      confirmText: '로그인',
+    };
 
-    return <Navigate to='/login' state={{ from: location }} replace />;
+    return (
+      <NoticeModal
+        modalText={modalText}
+        onConfirm={() =>
+          navigate('/login', { state: { from: location }, replace: true })
+        }
+      />
+    );
   }
 
   return children ? children : <Outlet />;
