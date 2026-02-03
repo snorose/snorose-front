880acc9e4880c6afb132bcc7b79d7e3b4fd1e047
haesa <34948133+haesa@users.noreply.github.com>
2026-02-04 00:33:03 +0900

    [SNO-265] \#1343 신고 및 문의 작성 페이지 UI (#1364)
    * #1343 [FEAT] 내부 상태를 공유하는 드롭다운 컴포넌트 개발(DropdownBlue)
    
    * #1343 [REFACTOR] 권한 상수 파일명 변경 role.js -> role.ts
    
    * #1343 [FEAT] TextFieldBlue 컴포넌트 개발
    
    인풋과 라벨은 논리적으로 항상 함께 움직이는 단위이기 때문에 이를 하나로 묶어 관리하기 위해 컴파운드 컴포넌트 패턴을 적용함, 또한 웹 접근성 강화를 위해 context로 id 관리
    
    * #1343 [FEAT] Profile 컴포넌트 개발
    
    * #1343 [FEAT] TextareaFieldBlue 컴포넌트 개발
    
    * #1343 [CHORE] svg, module.css 확장자에 대한 모듈 선언 추가
    
    * #1343 [FEAT] 역할에 대한 글로벌 타입 추가
    
    * #1343 [REFACTOR] clip-board-list 아이콘 교체
    
    * #1343 [FEAT] 신고 기능에 필요한 placeholder와 option에 대한 상수 및 데이터 추가
    
    * #1343 [FEAT] 신고 작성 페이지 UI 개발
    
    * #1343 [UI] 문의 작성 페이지 UI 개발
    
    * #1343 [FEAT] DropdownBlue 키보드 내비게이션 접근성 기능 추가
    
    * #1343 [REFACTOR] Option 타입을 글로벌 타입으로 분리
    
    * #1343 [REFACTOR] DropdownBlue 키보드 네비게이션 로직 수정 및 close 후 트리거로 포커스 회복
    
    * #1343 [FIX] 타입 별칭 참조 전 변수 선언으로 ReferenceError 방지
    
    * #1343 [FIX] 문의 작성 페이지 라벨 오타 수정
    
    * #1343 [FIX] TextariaFieldBlue 최대 글자 수 하드코딩을 prop으로 교체
    
    * #1343 [FIX] 문의 및 신고 작성 드롭다운 컴포넌트에 map key 누락
    
    * #1343 [REFACTOR] DropdownBlue 스타일을 부모 컴포넌트에서 제어할 수 있도록 변경
    
    * #1343 [FIX] FileUploadSection props 타입 명시
    
    * #1343 [REFACTOR] 신고 작성 페이지 드롭다운 스타일을 부모에서 제어하도록 변경
    
    * #1343 [FIX] 신고 placeholder 파일명 오타 수정
    
    * #1343 [FIX] 신고 및 문의 작성 드롭다운 라벨 공백 제거
    
    * #1343 [FIX] DropdownBlue 컴포넌트의 아이콘에서 불필요한 속성 제거
    
    * #1343 [REFACTOR] 신고 및 문의 작성 페이지에서 재사용 컴포넌트를 support/ui로 분리
    
    * #1343 [FIX] 문의 및 신고 작성 페이지에서 선택 된 모든 파일명 표시
    
    요소가 넘치면 말줄임표로 표시
    
    * [FIX] #1343 DropdownBlue 포커스 버그 수정
    
    선택된 메뉴에 focus가 자동으로 잡히지 않고, hover와 focus 스타일이 동시에 보여지는 문제를 수정했습니다.
    
    * [FIX] #1343 FileUploadSection 컴포넌트 포커스 활성화
    
    * [FIX] #1343 FileUploadSection 호버케이스 추가
    
    * [FIX] #1343 DropdownBlue.Menu 위치 조정

--- src/page/support/WriteInquiryPage/WriteInquiryPage.tsx
+++ src/page/support/WriteInquiryPage/WriteInquiryPage.tsx
@@ -1,3 +1,101 @@
+import { useState } from 'react';
+
+import { useAuth } from '@/shared/hook';
+import {
+  CloseAppBar,
+  DropdownBlue,
+  Profile,
+  TextareaFieldBlue,
+  TextFieldBlue,
+} from '@/shared/component';
+
+import { SubmitButton, FileUploadSection } from '@/feature/support/ui';
+import { INQUIRY_OPTION } from '@/feature/support/data';
+import { INQUIRY_PLACEHOLDERS } from '@/feature/support/constant';
+
+import { Option } from '@/types';
+
+import styles from './WriteInquiryPage.module.css';
+
 export default function WriteInquiryPage() {
-  return <div>WriteInquiryPage</div>;
+  const { userInfo } = useAuth();
+
+  const [selectedOption, setSelectedOption] = useState<Option | undefined>();
+  const [title, setTitle] = useState('');
+  const [url, setUrl] = useState('');
+  const [content, setContent] = useState('');
+  const [files, setFiles] = useState<File[]>([]);
+  const updateOption = (option: Option) => setSelectedOption(option);
+  const updateFiles = (files: File[]) => setFiles(files);
+
+  const disabled =
+    !selectedOption || title.trim() === '' || content.trim() === '';
+
+  if (!userInfo) {
+    return null;
+  }
+
+  return (
+    <div className={styles.container}>
+      <CloseAppBar backgroundColor={'#eaf5fd'}>
+        <SubmitButton onClick={() => alert('submit!')} disabled={disabled}>
+          등록
+        </SubmitButton>
+      </CloseAppBar>
+
+      <DropdownBlue className={styles.dropdown}>
+        <DropdownBlue.Trigger>
+          {selectedOption?.label ?? INQUIRY_PLACEHOLDERS.dropdown}
+        </DropdownBlue.Trigger>
+
+        <DropdownBlue.Menu>
+          {INQUIRY_OPTION.map((option) => (
+            <DropdownBlue.Item
+              key={option.key}
+              id={option.key}
+              selected={selectedOption?.key === option.key}
+              onClick={() => updateOption(option)}
+            >
+              {option.label}
+            </DropdownBlue.Item>
+          ))}
+        </DropdownBlue.Menu>
+      </DropdownBlue>
+
+      <Profile userRoleId={userInfo.userRoleId} nickname={userInfo.nickname} />
+
+      <div className={styles.form}>
+        <TextFieldBlue>
+          <TextFieldBlue.Label>제목</TextFieldBlue.Label>
+          <TextFieldBlue.Input
+            placeholder={INQUIRY_PLACEHOLDERS.title}
+            value={title}
+            onChange={(next) => setTitle(next)}
+          />
+        </TextFieldBlue>
+
+        <TextFieldBlue>
+          <TextFieldBlue.Label>게시글 링크</TextFieldBlue.Label>
+          <TextFieldBlue.Input
+            placeholder={INQUIRY_PLACEHOLDERS.url}
+            value={url}
+            onChange={(next) => setUrl(next)}
+          />
+        </TextFieldBlue>
+
+        <TextareaFieldBlue>
+          <TextareaFieldBlue.Label>문의 내용</TextareaFieldBlue.Label>
+          <TextareaFieldBlue.Input
+            placeholder={INQUIRY_PLACEHOLDERS.content}
+            value={content}
+            onChange={(next) => setContent(next)}
+            minRows={5}
+            maxRows={10}
+          />
+        </TextareaFieldBlue>
+
+        <FileUploadSection files={files} updateFiles={updateFiles} />
+      </div>
+    </div>
+  );
 }
