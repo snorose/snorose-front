d60f82b052c6f461c730a87e1d60b399382f7f93
haesa <xdhaesa@gmail.com>
2026-02-09 03:52:54 +0900

    [FEAT] #1387 게시판 조회 및 네비게이트 로직을 묶은 커스텀 훅

---
+++ src/shared/hook/board.hook.ts
@@ -0,0 +1,55 @@
+import { useLocation, useParams } from 'react-router-dom';
+
+import { BOARD_REGISTRY } from '@/shared/lib';
+import { NEW_ROUTES } from '@/shared/constant';
+
+import type { BoardKey } from '@/types';
+
+type BoardParams = {
+  boardKey: BoardKey;
+  postId: string;
+};
+
+export function useBoard() {
+  const { boardKey } = useParams<BoardParams>();
+  const { pathname } = useLocation();
+
+  const isGlobalNotice = pathname === NEW_ROUTES.globalNotice.list;
+  const notBoard = !boardKey && !isGlobalNotice;
+  if (notBoard) {
+    throw new Error('[useBoard] 게시판 페이지에서만 사용 가능한 훅입니다.');
+  }
+
+  const board = BOARD_REGISTRY.find(isGlobalNotice ? 'notice' : boardKey);
+
+  return { ...board, isGlobalNotice };
+}
+
+export function useBoardNavigate() {
+  const { boardKey, postId } = useParams<BoardParams>();
+
+  return {
+    toList: () => NEW_ROUTES.post.list(boardKey),
+    toWrite: () => NEW_ROUTES.post.write(boardKey),
+    toDetail: () => NEW_ROUTES.post.detail(boardKey, postId),
+    toEdit: () => NEW_ROUTES.post.edit(boardKey, postId),
+    toSearch: () => NEW_ROUTES.post.search(boardKey),
+
+    toNoticeList: (isGlobalNotice: boolean) =>
+      isGlobalNotice
+        ? NEW_ROUTES.globalNotice.list
+        : NEW_ROUTES.notice.list(boardKey),
+    toNoticeWrite: (isGlobalNotice: boolean) =>
+      isGlobalNotice
+        ? NEW_ROUTES.globalNotice.write
+        : NEW_ROUTES.notice.write(boardKey),
+    toNoticeDetail: (isGlobalNotice: boolean) =>
+      isGlobalNotice
+        ? NEW_ROUTES.globalNotice.detail(postId)
+        : NEW_ROUTES.notice.detail(boardKey, postId),
+    toNoticeEdit: (isGlobalNotice: boolean) =>
+      isGlobalNotice
+        ? NEW_ROUTES.globalNotice.edit(postId)
+        : NEW_ROUTES.notice.edit(boardKey, postId),
+  };
+}
