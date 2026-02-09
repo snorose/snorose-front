953a234400b4aaef2de00998a426e6e424e8cacc
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2026-02-09 12:13:49 +0900

    [FEAT] #1411 tiptap 기본 컴포넌트 생성

---
+++ src/shared/component/editor/TipTap.jsx
@@ -0,0 +1,22 @@
+import { useEditor, EditorContent } from '@tiptap/react';
+import { FloatingMenu, BubbleMenu } from '@tiptap/react/menus';
+import StarterKit from '@tiptap/starter-kit';
+
+import styles from './TipTap.module.css';
+
+const Tiptap = () => {
+  const editor = useEditor({
+    extensions: [StarterKit], // define your extension array
+    content: '<p>Hello World!</p>', // initial content
+  });
+
+  return (
+    <>
+      <EditorContent editor={editor} />
+      <FloatingMenu editor={editor}>This is the floating menu</FloatingMenu>
+      <BubbleMenu editor={editor}>This is the bubble menu</BubbleMenu>
+    </>
+  );
+};
+
+export default Tiptap;
