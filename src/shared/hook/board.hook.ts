d7ea269fe4a4e3781a8e4a4f0d5712dfd7de4684
haesa <34948133+haesa@users.noreply.github.com>
2026-02-09 22:06:57 +0900

    [SNO-286, SNO-287] #1387 게시판 훅(useBoard 및 useBoardNavigate) (#1412)
    * [RENAME] 페이지 경로 상수 파일 확장자를 ts로 변경(route.js -> route.ts)
    
    * [FEAT] #1387 게시판 조회 및 네비게이트 로직을 묶은 커스텀 훅
    
    * [FIX] #1387 useBoard - 자식 라우트에서도 전체공지 페이지임을 알 수 있도록 startWith으로 로직 변경
    
    * [REFACTOR] \#1387 useBoardNavigate - boardKey와 postId는 외부 주입
    
    * [REFACTOR] \#1387 useBoardNavigate - 전체 공지 여부를 훅 내부에서 판별
    
    * [FIX] \#1387 BOARD_REGISTRY.find 반환 타입에 undefined 추가 대신 fast-fail로 처리
    
    * [FIX] \#1387 정적 경로가 먼저 오도록 수정

---
+++ src/shared/hook/board.hook.ts
@@ -0,0 +1,64 @@
+import { useLocation, useParams } from 'react-router-dom';
+
+import { BOARD_REGISTRY } from '@/shared/lib';
+import { NEW_ROUTES } from '@/shared/constant';
+
+import type { BoardKey } from '@/types';
+
+type BoardParams = {
+  boardKey: BoardKey;
+};
+
+export function useBoard() {
+  const { boardKey } = useParams<BoardParams>();
+  const { pathname } = useLocation();
+
+  const isGlobalNotice = pathname.startsWith(NEW_ROUTES.globalNotice.list);
+  const notBoard = !boardKey && !isGlobalNotice;
+  if (notBoard) {
+    throw new Error('[useBoard] 게시판 페이지에서만 사용 가능한 훅입니다.');
+  }
+
+  const board = BOARD_REGISTRY.find(isGlobalNotice ? 'notice' : boardKey);
+
+  return { ...board, isGlobalNotice };
+}
+
+export function useBoardNavigate() {
+  const isGlobalNotice = (boardKey: BoardKey) => boardKey === 'notice';
+
+  const toNoticeList = (boardKey: BoardKey) =>
+    isGlobalNotice(boardKey)
+      ? NEW_ROUTES.globalNotice.list
+      : NEW_ROUTES.notice.list(boardKey);
+
+  const toNoticeWrite = (boardKey: BoardKey) =>
+    isGlobalNotice(boardKey)
+      ? NEW_ROUTES.globalNotice.write
+      : NEW_ROUTES.notice.write(boardKey);
+
+  const toNoticeDetail = (boardKey: BoardKey, postId: string) =>
+    isGlobalNotice(boardKey)
+      ? NEW_ROUTES.globalNotice.detail(postId)
+      : NEW_ROUTES.notice.detail(boardKey, postId);
+
+  const toNoticeEdit = (boardKey: BoardKey, postId: string) =>
+    isGlobalNotice(boardKey)
+      ? NEW_ROUTES.globalNotice.edit(postId)
+      : NEW_ROUTES.notice.edit(boardKey, postId);
+
+  return {
+    toNoticeList,
+    toNoticeWrite,
+    toNoticeDetail,
+    toNoticeEdit,
+
+    toList: (boardKey: BoardKey) => NEW_ROUTES.post.list(boardKey),
+    toWrite: (boardKey: BoardKey) => NEW_ROUTES.post.write(boardKey),
+    toDetail: (boardKey: BoardKey, postId: string) =>
+      NEW_ROUTES.post.detail(boardKey, postId),
+    toEdit: (boardKey: BoardKey, postId: string) =>
+      NEW_ROUTES.post.edit(boardKey, postId),
+    toSearch: (boardKey: BoardKey) => NEW_ROUTES.post.search(boardKey),
+  };
+}
