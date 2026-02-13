368eccafd016127c7807404afcf3e32c97943cd7
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2026-02-13 15:48:08 +0900

    [REFACTOR] #1416 현재 안 쓰는 코드 정리

--- src/shared/component/editor/Editor.jsx
+++ src/shared/component/editor/Editor.jsx
@@ -1,5 +1,4 @@
-import { useMemo } from 'react';
-import { useEditor, EditorContent, EditorContext } from '@tiptap/react';
+import { useEditor, EditorContent } from '@tiptap/react';
 import { FloatingMenu, BubbleMenu } from '@tiptap/react/menus';
 import { Placeholder } from '@tiptap/extensions';
 import StarterKit from '@tiptap/starter-kit';
@@ -14,20 +13,17 @@ export default function Editor({ placeholder }) {
         emptyEditorClass: 'is-editor-empty',
         placeholder,
       }),
-    ], // define your extension array
+    ],
     immediatelyRender: false,
   });
-
-  // Memoize the provider value to avoid unnecessary re-renders
-  const providerValue = useMemo(() => ({ editor }), [editor]);
 
   if (!editor) return null;
 
   return (
-    <EditorContext.Provider value={providerValue}>
+    <>
       <EditorContent editor={editor} />
       {/*<FloatingMenu editor={editor}>This is the floating menu</FloatingMenu>*/}
       <BubbleMenu editor={editor}>This is the bubble menu</BubbleMenu>
-    </EditorContext.Provider>
+    </>
   );
 }
