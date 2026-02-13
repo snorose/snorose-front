b4a9d55923eb48d9a0f52b90801e626ca811fc23
gain-0525 <kimgain0525@sookmyung.ac.kr>
2026-02-13 19:13:20 +0900

    [FEAT] 상단  툴바 기능 적용

--- src/shared/component/editor/Editor.jsx
+++ src/shared/component/editor/Editor.jsx
@@ -1,9 +1,10 @@
-import { useEditor, EditorContent, EditorContext } from '@tiptap/react';
+import { useEditor, EditorContent } from '@tiptap/react';
 import { FloatingMenu, BubbleMenu } from '@tiptap/react/menus';
 import { Placeholder } from '@tiptap/extensions';
 import StarterKit from '@tiptap/starter-kit';
 import TextAlign from '@tiptap/extension-text-align';
 import { TextStyle, Color, BackgroundColor, FontFamily } from '@tiptap/extension-text-style';
+import Image from '@tiptap/extension-image';
 import Toolbar from './Toolbar';
 import styles from './Editor.module.css';
 
@@ -11,11 +12,18 @@ export default function Editor({ placeholder, setText }) {
   const editor = useEditor({
     extensions: [
       StarterKit,
-      TextAlign.configure({ types: ['heading', 'paragraph'] }),
+      TextAlign.configure({ 
+        types: ['heading', 'paragraph'],
+        alignments: ['left', 'center', 'right'],
+      }),
       TextStyle,
       Color,
       BackgroundColor,
       FontFamily,
+      Image.configure({
+        inline: true,
+        allowBase64: true,
+      }),
       Placeholder.configure({
         emptyEditorClass: 'is-editor-empty',
         placeholder,
@@ -30,9 +38,11 @@ export default function Editor({ placeholder, setText }) {
   if (!editor) return null;
 
   return (
-    <EditorContext.Provider value={{ editor }}>
-      <Toolbar editor={editor} />
+    <>
       <EditorContent editor={editor} />
-    </EditorContext.Provider>
+      <Toolbar editor={editor} />
+      {/*<FloatingMenu editor={editor}>This is the floating menu</FloatingMenu>*/}
+      {/*<BubbleMenu editor={editor}>This is the bubble menu</BubbleMenu>*/}
+    </>
   );
 }
