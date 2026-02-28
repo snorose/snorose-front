582c8fce440b126edebcb22fecdddacff103e852
haesa <xdhaesa@gmail.com>
2026-02-28 09:06:04 +0900

    [FEAT] #1343 신고글 수정 페이지 UI

--- src/page/support/EditReportPage/EditReportPage.tsx
+++ src/page/support/EditReportPage/EditReportPage.tsx
@@ -1,12 +1,119 @@
-import { useParams } from 'react-router-dom';
+import { useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
+
+import { useAuth } from '@/shared/hook';
+import {
+  CloseAppBar,
+  DropdownBlue,
+  Profile,
+  TextareaFieldBlue,
+  TextFieldBlue,
+} from '@/shared/component';
+
+import { FileUploadSection, SubmitButton } from '@/feature/support/ui';
+import { REPORT_PLACEHOLDERS } from '@/feature/support/constant';
+import { REPORT_OPTION } from '@/feature/support/data';
+
+import { Option } from '@/types';
+import { ReportDTO } from '@/feature/support/types';
+
+import styles from './EditReportPage.module.css';
+
+const REPORT_TYPE_MAP = {
+  POST_REPORT: 'post',
+  EXAM_REVIEW_REPORT: 'exam',
+  COMMENT_REPORT: 'comment',
+  USER_REPORT: 'user',
+};
+
+const REPORT_TYPE_TAG = {
+  post: '게시글',
+  exam: '시험후기',
+  comment: '댓글',
+  user: '유저',
+};
 
 export default function EditReportPage() {
-  const { reportId } = useParams();
+  const post = useLoaderData() as ReportDTO;
+
+  const { userInfo } = useAuth();
+
+  const reportType = REPORT_TYPE_MAP[post.reportType];
+
+  const [selectedOption, setSelectedOption] = useState<Option>(() =>
+    REPORT_OPTION[reportType].find((option) => option.key === post.category)
+  );
+  const [title, setTitle] = useState(post.title);
+  const [content, setContent] = useState(post.content);
+  const [files, setFiles] = useState<File[]>(post.attachments);
+
+  const updateOption = (option: Option) => setSelectedOption(option);
+  const updateFiles = (files: File[]) => setFiles(files);
+
+  const placeholder = REPORT_PLACEHOLDERS[reportType];
+  const options = REPORT_OPTION[reportType];
+
+  const disabled = title.trim() === '' || content.trim() === '';
+
+  if (!userInfo) {
+    return null;
+  }
 
   return (
-    <div>
-      <div>EditReportPage</div>
-      <div>reportId: {reportId}</div>
+    <div className={styles.container}>
+      <CloseAppBar backgroundColor={'#eaf5fd'}>
+        <SubmitButton onClick={() => alert('submit!')} disabled={disabled}>
+          등록
+        </SubmitButton>
+      </CloseAppBar>
+
+      <DropdownBlue className={styles.dropdown}>
+        <DropdownBlue.Trigger>
+          <span className={styles.reportType}>
+            [{REPORT_TYPE_TAG[reportType]} 신고]
+          </span>
+          {selectedOption?.label ?? placeholder.dropdown}
+        </DropdownBlue.Trigger>
+
+        <DropdownBlue.Menu>
+          {options.map((option) => (
+            <DropdownBlue.Item
+              key={option.key}
+              id={option.key}
+              selected={selectedOption?.key === option.key}
+              onClick={() => updateOption(option)}
+            >
+              {option.label}
+            </DropdownBlue.Item>
+          ))}
+        </DropdownBlue.Menu>
+      </DropdownBlue>
+
+      <Profile userRoleId={userInfo.userRoleId} nickname={userInfo.nickname} />
+
+      <div className={styles.form}>
+        <TextFieldBlue>
+          <TextFieldBlue.Label>제목</TextFieldBlue.Label>
+          <TextFieldBlue.Input
+            placeholder={placeholder.title}
+            value={title}
+            onChange={(next) => setTitle(next)}
+          />
+        </TextFieldBlue>
+
+        <TextareaFieldBlue>
+          <TextareaFieldBlue.Label>신고 내용</TextareaFieldBlue.Label>
+          <TextareaFieldBlue.Input
+            placeholder={placeholder.content}
+            value={content}
+            onChange={(next) => setContent(next)}
+            minRows={5}
+            maxRows={10}
+          />
+        </TextareaFieldBlue>
+
+        <FileUploadSection files={files} updateFiles={updateFiles} />
+      </div>
     </div>
   );
 }
