0fa983aeb208dd9a6ebe3dbfa6d2bf8a801efc6b
dawncoding <sunnuri99@gmail.com>
2026-03-01 22:40:40 +0900

    [REFACTOR] #1446 EditorContainer 컴포넌트 위치 이동 및 컴포넌트명 변경

---
+++ src/feature/editor/component/EditorContainer/EditorContainer.jsx
@@ -0,0 +1,48 @@
+import { useEditor, EditorContent } from '@tiptap/react';
+import { Placeholder } from '@tiptap/extensions';
+import StarterKit from '@tiptap/starter-kit';
+import TextAlign from '@tiptap/extension-text-align';
+import {
+  TextStyle,
+  Color,
+  BackgroundColor,
+  FontFamily,
+} from '@tiptap/extension-text-style';
+import Image from '@tiptap/extension-image';
+
+import FixedMenuEditor from '../FixedMenuEditor/FixedMenuEditor';
+
+export default function EditorContainer({ placeholder, setText }) {
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      TextAlign.configure({
+        types: ['heading', 'paragraph'],
+        alignments: ['left', 'center', 'right'],
+      }),
+      TextStyle,
+      Color,
+      BackgroundColor,
+      FontFamily,
+      Image.configure({
+        inline: true,
+        allowBase64: true,
+      }),
+      Placeholder.configure({
+        emptyEditorClass: 'is-editor-empty',
+        placeholder,
+      }),
+    ],
+    immediatelyRender: false,
+    onUpdate: ({ editor }) => {
+      if (setText) setText(editor);
+    },
+  });
+
+  return (
+    <>
+      <FixedMenuEditor editor={editor} />
+      <EditorContent editor={editor} />
+    </>
+  );
+}
