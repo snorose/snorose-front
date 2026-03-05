4591c5384a216a795112d9f042b478086ca4a569
haesa <34948133+haesa@users.noreply.github.com>
2026-03-06 06:15:58 +0900

    [SNO-295] #1343 문의 및 신고 수정 페이지 UI (#1441)
    * [REFACTOR] #1343 문의글 옵션 상수 이름 변경 (INQUIRY_OPTION -> INQUIRY_OPTIONS)
    
    * [REFACTOR] #1343 문의글 옵션 ENUM을 백엔드와 동기화
    
    * [FEAT] #1343 문의글 수정 페이지에 loader 설정
    
    * [FEAT] #1343 문의글 수정 페이지 UI
    
    * [CHORE] #1343 TODO 주석 수정
    
    * [FEAT] #1343 문의 옵션 수정 가능
    
    * [CHORE] #1343 사용하지 않는 import 정리
    
    * [FEAT] #1343 ReportDTO 추가 및 types.ts 위치 변경
    
    * [RENAME] #1343 문의글 loader의 이름 및 위치 변경
    
    * [FEAT] #1343 신고글 수정 페이지 loader 추가
    
    * [FEAT] #1343 신고 옵션 ENUM 백엔드와 동기화
    
    * [FEAT] reportType, category 유니언 타입으로 지정
    
    * [CHORE] #1343 import 위치 정리
    
    * [FEAT] #1343 신고글 수정 페이지 UI
    
    * [RENAME] #1343 신고 옵션 상수 이름 변경 (REPORT_OPTION -> REPORT_OPTIONS)
    
    * [FEAT] #1343 Attachment 타입 추가
    
    * [REFACTOR] #1343 loader 함수 인자에 타입 주석 추가

--- src/feature/support/data/inquiry.ts
+++ src/feature/support/data/inquiry.ts
@@ -1,6 +1,6 @@
-export const INQUIRY_OPTION = [
-  { key: 'EXAM_REVIEW', label: '족보 관련 문의' },
-  { key: 'EVENT', label: '이벤트 관련 문의' },
-  { key: 'NOTICE', label: '공지 관련 문의' },
-  { key: 'OTHER', label: '기타' },
+export const INQUIRY_OPTIONS = [
+  { key: 'EXAM_REVIEW_INQUIRY', label: '족보 관련 문의' },
+  { key: 'EVENT_INQUIRY', label: '이벤트 관련 문의' },
+  { key: 'NOTICE_INQUIRY', label: '공지 관련 문의' },
+  { key: 'ETC_INQUIRY', label: '기타' },
 ];
--- src/page/support/EditInquiryPage/EditInquiryPage.tsx
+++ src/page/support/EditInquiryPage/EditInquiryPage.tsx
@@ -1,12 +1,108 @@
-import { useParams } from 'react-router-dom';
+import { useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
+
+import { useAuth } from '@/shared/hook';
+import {
+  CloseAppBar,
+  DropdownBlue,
+  Profile,
+  TextareaFieldBlue,
+  TextFieldBlue,
+} from '@/shared/component';
+
+import { SubmitButton, FileUploadSection } from '@/feature/support/ui';
+import { INQUIRY_OPTIONS } from '@/feature/support/data';
+import { INQUIRY_PLACEHOLDERS } from '@/feature/support/constant';
+
+import type { Option } from '@/types';
+import type { InquiryDTO } from '@/feature/support/types';
+import type { Attachment } from '@/feature/attachment/types';
+
+import styles from './EditInquiryPage.module.css';
 
 export default function EditInquiryPage() {
-  const { inquireId } = useParams();
+  const post = useLoaderData() as InquiryDTO;
+
+  const { userInfo } = useAuth();
+
+  const [selectedOption, setSelectedOption] = useState(() =>
+    INQUIRY_OPTIONS.find((option) => option.key === post.category)
+  );
+  const [title, setTitle] = useState(post.title);
+  const [url, setUrl] = useState(post.link);
+  const [content, setContent] = useState(post.content);
+  const [files, setFiles] = useState<(File | Attachment)[]>(post.attachments);
+
+  const updateOption = (option: Option) => setSelectedOption(option);
+  const updateFiles = (files: (File | Attachment)[]) => setFiles(files);
+
+  const disabled = title.trim() === '' || content.trim() === '';
+
+  if (!userInfo) {
+    return null;
+  }
 
   return (
-    <div>
-      <div>EditInquiryPage</div>
-      <div>inquireId: {inquireId}</div>
+    <div className={styles.container}>
+      <CloseAppBar backgroundColor={'#eaf5fd'}>
+        <SubmitButton onClick={() => alert('submit!')} disabled={disabled}>
+          등록
+        </SubmitButton>
+      </CloseAppBar>
+
+      <DropdownBlue className={styles.dropdown}>
+        <DropdownBlue.Trigger>
+          {selectedOption?.label ?? INQUIRY_PLACEHOLDERS.dropdown}
+        </DropdownBlue.Trigger>
+
+        <DropdownBlue.Menu>
+          {INQUIRY_OPTIONS.map((option) => (
+            <DropdownBlue.Item
+              key={option.key}
+              id={option.key}
+              selected={selectedOption?.key === option.key}
+              onClick={() => updateOption(option)}
+            >
+              {option.label}
+            </DropdownBlue.Item>
+          ))}
+        </DropdownBlue.Menu>
+      </DropdownBlue>
+
+      <Profile userRoleId={userInfo.userRoleId} nickname={userInfo.nickname} />
+
+      <div className={styles.form}>
+        <TextFieldBlue>
+          <TextFieldBlue.Label>제목</TextFieldBlue.Label>
+          <TextFieldBlue.Input
+            placeholder={INQUIRY_PLACEHOLDERS.title}
+            value={title}
+            onChange={(next) => setTitle(next)}
+          />
+        </TextFieldBlue>
+
+        <TextFieldBlue>
+          <TextFieldBlue.Label>게시글 링크</TextFieldBlue.Label>
+          <TextFieldBlue.Input
+            placeholder={INQUIRY_PLACEHOLDERS.url}
+            value={url}
+            onChange={(next) => setUrl(next)}
+          />
+        </TextFieldBlue>
+
+        <TextareaFieldBlue>
+          <TextareaFieldBlue.Label>문의 내용</TextareaFieldBlue.Label>
+          <TextareaFieldBlue.Input
+            placeholder={INQUIRY_PLACEHOLDERS.content}
+            value={content}
+            onChange={(next) => setContent(next)}
+            minRows={5}
+            maxRows={10}
+          />
+        </TextareaFieldBlue>
+
+        <FileUploadSection files={files} updateFiles={updateFiles} />
+      </div>
     </div>
   );
 }
