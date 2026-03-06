8258f0b56638737d834c57868369bfcc9508467c
김가인 <kimgain0525@sookmyung.ac.kr>
2026-03-07 01:55:05 +0900

    [REFACTOR] #1498 rebase 충돌 해결

--- src/feature/editor/component/EditorContainer/EditorContainer.jsx
+++ src/feature/editor/component/EditorContainer/EditorContainer.jsx
@@ -1,34 +1,52 @@
 import { useEffect } from 'react';
 import { useEditor, EditorContent } from '@tiptap/react';
+import { useState } from 'react';
 import Placeholder from '@tiptap/extension-placeholder';
 import StarterKit from '@tiptap/starter-kit';
 import TextAlign from '@tiptap/extension-text-align';
+import Blockquote from '@tiptap/extension-blockquote';
+import Link from '@tiptap/extension-link';
 import {
   TextStyle,
   Color,
   BackgroundColor,
   FontFamily,
 } from '@tiptap/extension-text-style';
-import Image from '@tiptap/extension-image';
-
+import { Table } from '@tiptap/extension-table';
+import { TableRow } from '@tiptap/extension-table';
+import { TableCell } from '@tiptap/extension-table';
+import { TableHeader } from '@tiptap/extension-table';
 import FixedMenuEditor from '../FixedMenuEditor/FixedMenuEditor';
+import styles from './EditorContainer.module.css';
+import { Iframe } from '@/feature/editor/component/extensions/IframeExtension';
 
 export default function EditorContainer({ placeholder, text, setText }) {
+  const [isInTable, setIsInTable] = useState(false);
   const editor = useEditor({
     extensions: [
-      StarterKit,
+      StarterKit.configure({
+        link: false,
+        blockquote: false,
+      }),
+      Link.configure({
+        openOnClick: false,
+      }),
+      Blockquote,
       TextAlign.configure({
         types: ['heading', 'paragraph'],
         alignments: ['left', 'center', 'right'],
       }),
+      Table.configure({
+        resizable: true,
+      }),
+      Iframe,
+      TableRow,
+      TableHeader,
+      TableCell,
       TextStyle,
       Color,
       BackgroundColor,
       FontFamily,
-      Image.configure({
-        inline: true,
-        allowBase64: true,
-      }),
       Placeholder.configure({
         emptyEditorClass: 'is-editor-empty',
         placeholder,
@@ -37,6 +55,9 @@ export default function EditorContainer({ placeholder, text, setText }) {
     immediatelyRender: false,
     onUpdate: ({ editor }) => {
       if (setText) setText(editor);
+    },
+    onSelectionUpdate: ({ editor }) => {
+      setIsInTable(editor.isActive('table'));
     },
   });
 
@@ -51,6 +72,30 @@ export default function EditorContainer({ placeholder, text, setText }) {
     <>
       <FixedMenuEditor editor={editor} />
       <EditorContent editor={editor} />
+
+      {isInTable && (
+        <div type="button" className={styles.tableControls}>
+          <button onClick={() => editor.chain().focus().addRowAfter().run()}>
+            ➕ 행 추가
+          </button>
+          <button type="button" onClick={() => editor.chain().focus().addColumnAfter().run()}>
+            ➕ 열 추가
+          </button>
+          <button type="button" onClick={() => editor.chain().focus().deleteRow().run()}>
+            ➖ 행 삭제
+          </button>
+          <button type="button" onClick={() => editor.chain().focus().deleteColumn().run()}>
+            ➖ 열 삭제
+          </button>
+          <button 
+            type="button"
+            className={styles.deleteButton}
+            onClick={() => editor.chain().focus().deleteTable().run()}
+          >
+            🗑️ 표 삭제
+          </button>
+        </div>
+      )}
     </>
   );
 }
