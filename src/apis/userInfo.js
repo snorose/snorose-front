83f6957ce53f1350fdf6817ea9fb4a537d66abb0
haesa <xdhaesa@gmail.com>
2026-03-08 15:25:19 +0900

    [FEAT] #1436 내 문의 및 신고 모아보기 더미 데이터 추가

--- src/apis/userInfo.js
+++ src/apis/userInfo.js
@@ -51,11 +51,40 @@ export const getDownloadedExamReviews = async (params = {}) => {
 };
 
 export const getMyInquiriesAndReports = async (params = {}) => {
-  const response = await authAxios.get('/v1/users/mypage/inquiries', {
-    params,
-  });
+  // const response = await authAxios.get('/v1/users/mypage/inquiries', {
+  //   params,
+  // });
 
-  return response?.data.result;
+  // return response?.data.result;
+
+  return {
+    hasNext: true,
+    data: [
+      {
+        postId: 11,
+        boardId: 13,
+        title: '시험후기 신고합니다',
+        userDisplay: '정회원test',
+        createdAt: '2025-04-13T22:30:11.519393',
+        isEdited: true,
+        status: 'PENDING',
+        group: 'REPORT',
+        category: 'EXAM_FALSE_REVIEW',
+        reportType: 'EXAM',
+      },
+      {
+        postId: 1724902,
+        boardId: 13,
+        title: '이벤트 문의',
+        userDisplay: '익명송이',
+        createdAt: '2026-03-07T12:13:32.257872',
+        isEdited: false,
+        status: 'COMPLETED',
+        group: 'INQUIRY',
+        category: 'ECT',
+      },
+    ],
+  };
 };
 
 export const getScrapedExamReviews = async (params) => {
