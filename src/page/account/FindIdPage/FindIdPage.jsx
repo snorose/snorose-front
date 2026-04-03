b1e1ff1ecfde89f32a4616a2b9ad71a2b79dd008
하지민 <163178666+JiiminHa@users.noreply.github.com>
2026-04-03 13:51:51 +0900

    [STYLE] #1511 아이디 찾기 페이지 디자인 수정 사항 반영

--- src/page/account/FindIdPage/FindIdPage.jsx
+++ src/page/account/FindIdPage/FindIdPage.jsx
@@ -105,30 +105,37 @@ export default function FindIdPage() {
                 <li>
                   마이페이지에서 학번이 ‘학번+숫자’형식이에요. 변경하고 싶어요.
                 </li>
-                <div className={`${styles.highlight} ${styles.alertArrow}`}>
-                  → 구 스노로즈에 저장된 학번이 아래와 같은 경우,
-                  <br /> 데이터 정합성을 위하여 임의로 '학번+숫자'로
+              </ol>
+              <div className={styles.highlight}>
+                <Icon id='chevron-right' width={20} height={20} />
+                <span>
+                  구 스노로즈에 저장된 학번이 아래와 같은 경우,
+                  <br /> 데이터 정합성을 위하여 임의로 ‘학번+숫자’로
                   수정되었어요.
-                </div>
+                </span>
+              </div>
+              <ol>
                 <li>
                   가입 시 입력한 학번이 정확하지 않은 경우(ex 111111111111 등)
                 </li>
                 <li>다른 유저가 같은 학번으로 입학한 경우</li>
-                <div className={`${styles.highlight} ${styles.alertArrow}`}>
-                  → 하나의 학번 당 한 유저만 가입 가능하므로 리자가 임의로 다른
-                  학번으로 변경했어요.
-                </div>
-
-                <div className={styles.highlight}>
-                  따라서, 위의 경우에 해당하거나 학번이 잘못되어 수정이
-                  필요하다면, 아래 구글 폼을 통해 학번 변경 문의를 남겨주시면
-                  신속히 해결해드릴게요
-                </div>
-                <button className={styles.googleFormBtn}>
-                  <Icon id='google-form' width={'1.6rem'} height={'1.6rem'} />
-                  <a href='https://forms.gle/xwi7q47Dz59UNeEW7'>구글 폼</a>
-                </button>
               </ol>
+              <div className={styles.highlight}>
+                <Icon id='chevron-right' width={20} height={20} />
+                <span>
+                  하나의 학번 당 한 유저만 가입 가능하므로 리자가 임의로 다른
+                  학번으로 변경했어요.
+                </span>
+              </div>
+              <div className={styles.note}>
+                따라서, 위의 경우에 해당하거나 학번이 잘못되어 수정이
+                필요하다면, 아래 구글 폼을 통해 학번 변경 문의를 남겨주시면
+                신속히 해결해드릴게요
+              </div>
+              <button className={styles.googleFormBtn}>
+                <Icon id='google-form' width='1.6rem' height='1.6rem' />
+                <a href='https://forms.gle/xwi7q47Dz59UNeEW7'>구글 폼</a>
+              </button>
             </div>
           </div>
 
