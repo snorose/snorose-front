007ce6ecd89ae465f2b711d9e9497ac680a64e4d
하지민 <163178666+JiiminHa@users.noreply.github.com>
2026-04-04 19:52:15 +0900

    [STYLE] #1515 마케팅 정보 수신 동의 약관 페이지 타이틀 상단 고정

--- src/page/alert/MarketingTermsPage/MarketingTermsPage.jsx
+++ src/page/alert/MarketingTermsPage/MarketingTermsPage.jsx
@@ -7,7 +7,7 @@ import styles from './MarketingTermsPage.module.css';
 export default function MarketingTermsPage() {
   return (
     <div className={styles.contianer}>
-      <BackAppBar notFixed />
+      <BackAppBar />
 
       <div className={styles.title}>마케팅 정보 수신 동의 약관</div>
 
