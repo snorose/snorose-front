86112fc17cc73188a3a5eef01bd5f4bce67f1750
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2026-04-05 12:26:45 +0900

    [FEAT] #1520 비밀번호 찾기에 아이디 인풋창 문자 개수 제한 해제

--- src/feature/account/lib/inputCheck.js
+++ src/feature/account/lib/inputCheck.js
@@ -29,14 +29,24 @@ export function validateSookmyungEmail(value = '') {
 }
 
 export function validateId(value = '') {
+  const pathname = window.location.pathname.split('/')[1];
+
   const format = /^[A-Za-z0-9]+$/;
 
   if (value.length === 0) {
     return 'default';
   }
 
-  if (format.test(value) && value.length >= 5 && value.length <= 30) {
-    return 'valid';
+  if (pathname === 'signup') {
+    //회원가입에서는 문자 개수 제한 validate 적용
+    if (format.test(value) && value.length >= 5 && value.length <= 30) {
+      return 'valid';
+    }
+  } else if (pathname === 'find-pw') {
+    //비밀번호찾기에서는 문자 개수 제한 validate 적용 안 함 (과거 회원 고려)
+    if (format.test(value)) {
+      return 'valid';
+    }
   }
 
   return 'error';
