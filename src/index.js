611a03287319d68f0415c234bfc4ff39de74900a
haesa <xdhaesa@gmail.com>
2026-04-07 04:00:39 +0900

    [CHORE] #1531 문의 작성 API mocking

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import { GrowthBookProvider } from '@growthbook/growthbook-react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { GrowthBookProvider } from '@growthbook/growthbook-react';
 
 import { ModalProvider } from '@/shared/context/ModalContext';
 import { ToastProvider } from '@/shared/context/ToastContext';
 import { growthbook } from '@/shared/lib';
+
 import { CommentContextProvider } from '@/feature/comment/context';
 
-import { routeList } from '@/router.js';
 // import { routeList } from '@/router.migration.js';
 import reportWebVitals from '@/reportWebVitals';
+import { routeList } from '@/router.js';
+
 import '@/index.css';
 
 const router = createBrowserRouter(routeList);
@@ -29,22 +31,31 @@ const queryClient = new QueryClient({
 
 growthbook.init({ streaming: true });
 
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') return;
+
+  const { worker } = await import('@/mock/browser');
+  return worker.start();
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <GrowthBookProvider growthbook={growthbook}>
-      <QueryClientProvider client={queryClient}>
-        <ToastProvider>
-          <ModalProvider>
-            <CommentContextProvider>
-              <RouterProvider router={router} />
-            </CommentContextProvider>
-          </ModalProvider>
-        </ToastProvider>
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </GrowthBookProvider>
-  </React.StrictMode>
-);
+enableMocking().then(() => {
+  root.render(
+    <React.StrictMode>
+      <GrowthBookProvider growthbook={growthbook}>
+        <QueryClientProvider client={queryClient}>
+          <ToastProvider>
+            <ModalProvider>
+              <CommentContextProvider>
+                <RouterProvider router={router} />
+              </CommentContextProvider>
+            </ModalProvider>
+          </ToastProvider>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </GrowthBookProvider>
+    </React.StrictMode>
+  );
+});
 
 reportWebVitals();
