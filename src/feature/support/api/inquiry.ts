1bb682ba6338627d83e016da633199a453f66843
haesa <xdhaesa@gmail.com>
2026-04-14 01:13:12 +0900

    [FEAT] #1535 문의 작성 API 예외처리

--- src/feature/support/api/inquiry.ts
+++ src/feature/support/api/inquiry.ts
@@ -18,26 +18,43 @@ export const createInquiry = async ({
   target,
   attachments = [],
 }: InquiryWriteRequest) => {
-  const response = await authAxios.post('/v1/inquiries/inquiry', {
-    title,
-    target,
-    content,
-    inquiryCategory,
-    attachments: attachments.map(({ fileName, fileComment, type }) => ({
-      fileName,
-      fileComment,
-      type,
-    })),
-  });
+  try {
+    const response = await authAxios.post('/v1/inquiries/inquiry', {
+      title,
+      target,
+      content,
+      inquiryCategory,
+      attachments: attachments.map(({ fileName, fileComment, type }) => ({
+        fileName,
+        fileComment,
+        type,
+      })),
+    });
 
-  const { postId, attachmentUrlList } = response.data.result;
+    const { postId, attachmentUrlList } = response.data.result;
 
-  if (attachmentUrlList.length > 0) {
-    const files = attachments.map(({ file }) => file);
-    await putFileInBucket(attachmentUrlList, files);
+    if (attachmentUrlList.length > 0) {
+      await putFileInBucket(
+        attachmentUrlList,
+        attachments.map(({ file }) => file)
+      );
+    }
+
+    return { postId };
+  } catch (error) {
+    const { response } = error;
+
+    if (!response) {
+      throw new Error('네트워크 통신 중 오류가 발생했어요');
+    }
+
+    switch (response.status) {
+      case 403:
+        throw new Error('문의 작성 권한이 없어요');
+      default:
+        throw new Error('잠시 후 다시 시도해주세요.');
+    }
   }
-
-  return { postId };
 };
 
 export const fetchInquiry = async (inquiryId) => {
