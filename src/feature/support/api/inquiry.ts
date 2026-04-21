4722c634f9e0f2ac8094108588ce5adff101abd4
haesa <xdhaesa@gmail.com>
2026-04-21 19:52:01 +0900

    [FEAT] 문의 조회 API 연결 및 에러 핸들링

--- src/feature/support/api/inquiry.ts
+++ src/feature/support/api/inquiry.ts
@@ -3,6 +3,12 @@ import type { Attachment } from '@/feature/attachment/types';
 import { putFileInBucket } from '@/apis';
 import { authAxios } from '@/axios';
 
+export const readInquiry = async (inquiryId: number) => {
+  const response = await authAxios.get(`/v1/inquiries/${inquiryId}`);
+
+  return response.data.result;
+};
+
 export type InquiryCreateRequest = {
   title: string;
   content: string;
@@ -57,31 +63,4 @@ export const createInquiry = async ({
         throw new Error('잠시 후 다시 시도해주세요.');
     }
   }
-};
-
-export const readInquiry = async (inquiryId) => {
-  // const response = await authAxios.get(`/v1/inquiries/${inquiryId}`);
-
-  // return response?.data.result;
-
-  return {
-    postId: 15,
-    userRoleId: 2,
-    isWriter: true,
-    userId: '629j3YCdF2F+NPCBzMf0Rg==',
-    userDisplay: '눈송',
-    title: '이벤트 문의',
-    link: 'https://www.snorose.com/...',
-    content: '문의 내용',
-    category: 'EXAM_REVIEW_INQUIRY',
-    status: 'COMPLETED',
-    commentCount: 0,
-    likeCount: 0,
-    scrapCount: 0,
-    createdAt: '2025-03-07T12:13:09.234619',
-    updatedAt: null,
-    isEdited: false,
-    isWriterWithdrawn: false,
-    attachments: [],
-  };
 };
