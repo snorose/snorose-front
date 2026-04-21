4b099bb97625de25faf28471f3d2b71c352491ea
haesa <xdhaesa@gmail.com>
2026-04-21 20:51:01 +0900

    [FEAT] #1554 문의 및 신고 API 연결

--- src/apis/userInfo.js
+++ src/apis/userInfo.js
@@ -50,41 +50,12 @@ export const getDownloadedExamReviews = async (params = {}) => {
   return response?.data.result;
 };
 
-export const getMyInquiriesAndReports = async (params = {}) => {
-  // const response = await authAxios.get('/v1/users/mypage/inquiries', {
-  //   params,
-  // });
+export const readInquiryAndReportList = async (params) => {
+  const response = await authAxios.get('/v1/users/mypage/inquiries', {
+    params,
+  });
 
-  // return response?.data.result;
-
-  return {
-    hasNext: true,
-    data: [
-      {
-        postId: 1724829,
-        boardId: 13,
-        title: '악의적인 댓글 신고',
-        userDisplay: '눈송',
-        createdAt: '2025-08-30T22:47:11.519393',
-        isEdited: true,
-        status: 'PENDING',
-        group: 'REPORT',
-        category: 'EXAM_FALSE_REVIEW',
-        reportType: 'EXAM',
-      },
-      {
-        postId: 1724902,
-        boardId: 13,
-        title: '이벤트 문의',
-        userDisplay: '눈송',
-        createdAt: '2026-03-07T12:13:32.257872',
-        isEdited: false,
-        status: 'COMPLETED',
-        group: 'INQUIRY',
-        category: 'ECT',
-      },
-    ],
-  };
+  return response?.data.result;
 };
 
 export const getScrapedExamReviews = async (params) => {
