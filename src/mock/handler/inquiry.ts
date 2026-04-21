7f386e3bfe22c5036e1a6f4209c73297a7e99dbe
haesa <xdhaesa@gmail.com>
2026-04-21 20:50:46 +0900

    [CHORE] 문의 및 신고 리스트 조회 API Mocking

--- src/mock/handler/inquiry.ts
+++ src/mock/handler/inquiry.ts
@@ -3,6 +3,53 @@ import { http, HttpResponse } from 'msw';
 import type { InquiryWriteRequest } from '@/feature/support/api';
 
 export const inquiryHandlers = [
+  http.get('*/v1/users/mypage/inquiries', async () => {
+    // 성공
+    return HttpResponse.json({
+      isSuccess: true,
+      code: 1000,
+      message: '요청에 성공하였습니다.',
+      result: {
+        hasNext: false,
+        data: [
+          {
+            postId: 11,
+            title: '시험후기 신고합니다',
+            userDisplay: '정회원test',
+            createdAt: '2025-04-13T22:30:11.519393',
+            isEdited: true,
+            status: 'PENDING',
+            group: 'REPORT',
+            category: 'EXAM_FALSE_REVIEW',
+            reportType: 'EXAM',
+          },
+          {
+            postId: 111,
+            title: '이벤트 문의',
+            userDisplay: '눈송',
+            createdAt: '2025-04-21T12:30:11.519393',
+            isEdited: true,
+            status: 'PENDING',
+            group: 'REPORT',
+            category: 'EXAM_FALSE_REVIEW',
+            reportType: 'EXAM',
+          },
+        ],
+      },
+    });
+    // 실패 - 서버 오류
+    // return HttpResponse.json(
+    //   {
+    //     isSuccess: false,
+    //     code: 3031,
+    //     message: '해당 게시글을 찾을 수 없습니다',
+    //   },
+    //   { status: 500 }
+    // );
+    // 실패 - 네트워크 오류
+    // return HttpResponse.error();
+  }),
+
   http.post('*/v1/inquiries/inquiry', async ({ request }) => {
     const data = await request.json();
     const body = data as InquiryWriteRequest;
