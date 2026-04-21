8e1e570f70376c93f9d3f91bfd258aa655e78c52
haesa <xdhaesa@gmail.com>
2026-04-21 19:43:08 +0900

    [CHORE] 문의 조회 API mocking

--- src/mock/handler/inquiry.ts
+++ src/mock/handler/inquiry.ts
@@ -3,6 +3,62 @@ import { http, HttpResponse } from 'msw';
 import type { InquiryCreateRequest } from '@/feature/support/api';
 
 export const inquiryHandlers = [
+  http.get('*/v1/inquiries/111', async () => {
+    // 성공
+    return HttpResponse.json({
+      isSuccess: true,
+      code: 1000,
+      message: '요청에 성공하였습니다.',
+      result: {
+        postId: 15,
+        userRoleId: 4,
+        isWriter: false,
+        userId: '629j3YCdF2F+NPCBzMf0Rg==',
+        userDisplay: '눈송',
+        title: '시험후기 문의',
+        link: 'https://www.snorose.com/...',
+        content: '문의 내용',
+        category: 'EXAM_REVIEW_INQUIRY',
+        status: 'PENDING',
+        commentCount: 0,
+        createdAt: '2025-08-30T22:47:09.234619',
+        updatedAt: null,
+        isEdited: true,
+        isWriterWithdrawn: false,
+        attachments: [],
+      },
+    });
+    // 실패 - 권한 없음
+    // return HttpResponse.json(
+    //   {
+    //     isSuccess: false,
+    //     code: 2007,
+    //     message: '유저의 권한이 없습니다.',
+    //   },
+    //   { status: 403 }
+    // );
+    // 실패 - 없거나 삭제된 경우
+    // return HttpResponse.json(
+    //   {
+    //     isSuccess: false,
+    //     code: 3031,
+    //     message: '해당 게시글을 찾을 수 없습니다',
+    //   },
+    //   { status: 404 }
+    // );
+    // 실패 - 서버 오류
+    // return HttpResponse.json(
+    //   {
+    //     isSuccess: false,
+    //     code: 3031,
+    //     message: '해당 게시글을 찾을 수 없습니다',
+    //   },
+    //   { status: 500 }
+    // );
+    // 실패 - 네트워크 오류
+    // return HttpResponse.error();
+  }),
+
   http.post('*/v1/inquiries/inquiry', async ({ request }) => {
     const data = await request.json();
     const body = data as InquiryCreateRequest;
