f61daa826c4cf17bc706817c183bfe101d3daa39
gain-0525 <kimgain0525@sookmyung.ac.kr>
2026-04-25 18:57:51 +0900

    [FIX] #1540 카카오맵 임베드 사이트 추가

--- src/feature/editor/lib/sanitize.js
+++ src/feature/editor/lib/sanitize.js
@@ -5,7 +5,7 @@ export const sanitizeHtml = (html) => {
     if (node.tagName === 'IFRAME') {
       const src = node.getAttribute('src') || '';
 
-      const iframeAllowed = /^https?:\/\/((www\.)?youtube\.com\/embed\/|(www\.)?instagram\.com\/(p|reel)\/[a-zA-Z0-9_-]+\/embed\/|(www\.)?tiktok\.com\/embed\/v2\/|platform\.twitter\.com\/embed\/Tweet\.html\?id=|(www\.)?tv\.naver\.com\/embed\/|maps\.google\.com\/maps\?.*output=embed|(www\.)?google\.com\/maps\/embed|(map\.)?naver\.com\/|naver\.me\/)/.test(src);
+      const iframeAllowed = /^https?:\/\/((www\.)?youtube\.com\/embed\/|(www\.)?instagram\.com\/(p|reel)\/[a-zA-Z0-9_-]+\/embed\/|(www\.)?tiktok\.com\/embed\/v2\/|platform\.twitter\.com\/embed\/Tweet\.html\?id=|(www\.)?tv\.naver\.com\/embed\/|maps\.google\.com\/maps\?.*output=embed|(www\.)?google\.com\/maps\/embed|(map\.)?naver\.com\/|naver\.me\/|kko\.to\/)/.test(src);
       
       if (!iframeAllowed) {
         node.setAttribute('src', '');
