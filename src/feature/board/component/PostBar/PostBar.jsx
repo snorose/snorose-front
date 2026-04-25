bfb6f4b96da8760f55f55c6b0d3bd952406ef732
misung.dev <misung.dev@gmail.com>
2026-04-25 16:03:02 +0900

    [FIX] #1562 게시글 바 내에 메타 정보가 많을 경우 줄바꿈 되는 이슈 해결

--- src/feature/board/component/PostBar/PostBar.jsx
+++ src/feature/board/component/PostBar/PostBar.jsx
@@ -28,21 +28,20 @@ export default function PostBar({
 }) {
   return (
     <div className={`${styles.container} ${className}`}>
-      <div className={styles.body}>
-        <div>
-          <Meta
-            userRoleId={userRoleId}
-            userDisplay={userDisplay}
-            createdAt={createdAt}
-            isEdited={isEdited}
-          >
-            {children}
-          </Meta>
+      <Meta
+        userRoleId={userRoleId}
+        userDisplay={userDisplay}
+        createdAt={createdAt}
+        isEdited={isEdited}
+      >
+        {children}
+      </Meta>
 
+      <div className={styles.body}>
+        <div className={styles.text}>
           <div className={styles.title}>{title}</div>
           <div className={styles.content}>{content}</div>
         </div>
-
         {hasMediaAttachment && <Thumbnail thumbnailUrl={thumbnailUrl} />}
       </div>
 
@@ -61,15 +60,16 @@ function Meta({ userRoleId, userDisplay, createdAt, isEdited, children }) {
   const showBadge =
     userRoleId === ROLE.official ||
     (userRoleId === ROLE.admin && userDisplay !== '익명송이');
+  const truncatedUserDisplay =
+    userDisplay?.length > 6 ? `${userDisplay.slice(0, 6)}...` : userDisplay;
 
   return (
     <div className={styles.meta}>
       <img className={styles.cloudLogoIcon} src={cloudLogo} alt='로고' />
-      <div>{userDisplay}</div>
+      <div>{truncatedUserDisplay}</div>
       {showBadge && <Badge className={styles.badge} userRoleId={userRoleId} />}
       <div className={styles.dot}>·</div>
       <div>{DateTime.formatAdaptive(createdAt)}</div>
-      {isEdited && <div>(수정됨)</div>}
       {children}
     </div>
   );
