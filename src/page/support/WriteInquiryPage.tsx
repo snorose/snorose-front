a02e3e649ce6280249bdec04439beb0cfe63e117
haesa <34948133+haesa@users.noreply.github.com>
2026-04-29 19:31:29 +0900

    [SNO-394] #1560 문의 작성, 신고 작성 뷰 분리 (#1584)
    * [FIX] #1559 파일 연속 선택이 안 되는 문제 수정
    
    * [REFACTOR] #1599 문의 및 신고 작성, 수정 에디터 뷰 분리
    
    * [FIX] #1560 SupportFormView 수정 페이지에서 누락된 데이터 초기화 수행
    
    * [REFACTOR] #1560 게시글 링크 필드 placeholder를 상수에서 제거
    
    * [REFACTOR] #1560 attachment, file이 있을 경우에만 렌더링
    
    * [FIX] #1560 문법 오류 수정
    
    * [FIX] #1560 문법 오류 수정
    
    * [REFACTOR] #1560 오타 수정
    
    * [REFACTOR] #1560 selectedOption 타입 단언
    
    * [FEAT] #1560 파일 선택 해제 UI에 접근성 기능 추가
    
    * [FIX] #1560 prop 이름 불일치 수정

---
+++ src/page/support/WriteInquiryPage.tsx
@@ -0,0 +1,59 @@
+import { useNavigate } from 'react-router-dom';
+
+import { useMutation } from '@tanstack/react-query';
+
+import { BOARD_ID } from '@/shared/constant';
+import { useToast } from '@/shared/hook';
+
+import { mapFileToAttachment } from '@/feature/attachment/lib';
+import { createInquiry } from '@/feature/support/api';
+import { INQUIRY_PLACEHOLDERS } from '@/feature/support/constant';
+import { INQUIRY_OPTIONS } from '@/feature/support/data';
+import { SupportFormView } from '@/feature/support/ui';
+
+import { createThumbnail } from '@/apis';
+
+export default function WriteInquiryPage() {
+  const navigate = useNavigate();
+  const { toast } = useToast();
+
+  const { mutate: submitInquiry } = useMutation({
+    mutationFn: createInquiry,
+    onSuccess: (data) => {
+      const { postId } = data;
+
+      navigate(`/inquiry/${postId}`, { replace: true });
+
+      createThumbnail(BOARD_ID.inquiryAndReport, postId) //
+        .catch((error) => {
+          /**
+           * TODO: 썸네일 생성 실패는 치명적이지 않으므로, 에러를 사용자에게 알리지 않고 조용히 실패 처리합니다.
+           * 에러 로그 수집 (모니터링: sentry)
+           */
+        });
+    },
+    onError: (error) => {
+      toast({ message: error.message, variant: 'error' });
+    },
+  });
+
+  const { dropdown, title, content } = INQUIRY_PLACEHOLDERS;
+
+  return (
+    <SupportFormView
+      submit={({ title, content, selectedOption, url, files }) =>
+        submitInquiry({
+          title,
+          content,
+          inquiryCategory: selectedOption.key,
+          target: url,
+          attachments: files.map(mapFileToAttachment),
+        })
+      }
+      options={INQUIRY_OPTIONS}
+      contentLabel={'문의 내용'}
+      placeholders={{ dropdown, title, content }}
+      showLinkField
+    />
+  );
+}
