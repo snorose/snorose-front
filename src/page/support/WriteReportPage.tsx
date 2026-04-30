cf763ed2556b0e9410ab20c28874c23600635e5c
haesa <34948133+haesa@users.noreply.github.com>
2026-05-01 05:23:26 +0900

    [SNO-362] #1591 신고 작성 API 연동 (#1595)
    * [REFACTOR] #1591 ReportDetailPage import문 정리
    
    * [CHORE] #1591 신고 작성 API Mocking
    
    * [FEAT] 신고 작성 API 연동
    
    * [REFACTOR] #1591 사용하지 않는 report 도메인 폴더 삭제
    
    * [FIX] #1591 신고 네비게이트 조건 오타 수정
    
    * [REFACTOR] #1591 신고 작성 응답 타입 export

--- src/page/support/WriteReportPage.tsx
+++ src/page/support/WriteReportPage.tsx
@@ -1,22 +1,64 @@
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate, useSearchParams } from 'react-router-dom';
 
+import { useMutation } from '@tanstack/react-query';
+
+import { BOARD_ID } from '@/shared/constant';
+import { useToast } from '@/shared/hook';
+
+import { mapFileToAttachment } from '@/feature/attachment/lib';
+import { createReport } from '@/feature/support/api';
 import {
   REPORT_PLACEHOLDERS,
   REPORT_TYPE_TAG,
 } from '@/feature/support/constant';
-import { REPORT_OPTIONS } from '@/feature/support/data';
-import type { ReportParamsLoaderData } from '@/feature/support/loader';
+import { REPORT_OPTIONS, ReportType } from '@/feature/support/data';
 import { SupportFormView } from '@/feature/support/ui';
 
+import { createThumbnail } from '@/apis';
+
 export default function WriteReportPage() {
-  const { reportType, reportParams } =
-    useLoaderData() as ReportParamsLoaderData;
+  const { reportType } = useLoaderData() as { reportType: ReportType };
+
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+
+  const { toast } = useToast();
+
+  const { mutate: submit } = useMutation({
+    mutationFn: createReport,
+    onSuccess: (data) => {
+      const { postId } = data;
+
+      navigate(`/report/${postId}`, { replace: true });
+
+      createThumbnail(BOARD_ID.inquiryAndReport, postId) //
+        .catch((error) => {
+          /**
+           * TODO: 썸네일 생성 실패는 치명적이지 않으므로, 에러를 사용자에게 알리지 않고 조용히 실패 처리합니다.
+           * 에러 로그 수집 (모니터링: sentry)
+           */
+        });
+    },
+    onError: (error) => {
+      toast({ message: error.message, variant: 'error' });
+    },
+  });
 
   const { dropdown, title, content } = REPORT_PLACEHOLDERS[reportType];
+  const targetId = searchParams.get('targetId');
 
   return (
     <SupportFormView
-      submit={() => alert('submit!')}
+      submit={({ title, content, selectedOption, files }) =>
+        submit({
+          reportType,
+          targetId,
+          title,
+          content,
+          reportCategory: selectedOption.key!,
+          attachments: files.map(mapFileToAttachment),
+        })
+      }
       options={REPORT_OPTIONS[reportType]}
       contentLabel={'신고 내용'}
       placeholders={{ dropdown, title, content }}
