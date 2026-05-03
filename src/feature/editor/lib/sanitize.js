0ff4c0839dcb95e4c996f3e9ba6378206df70623
dawncoding <sunnuri99@gmail.com>
2026-05-04 00:57:42 +0900

    [REFACTOR] #1574 sanitize 코드 리팩토링
    - 하드코딩 배열 제거 : constant/embed-sources.js의 EMBED_SOURCES에서 embedUrlPattern 활용
    - isAllowedEmbedUrl 함수 생성 : EditorContainer에서 동일한 기준으로 버블 메뉴 노출 여부 판단할 수 있도록 생성 -> 작성하는 경우와 조회하는 경우의 비대칭 해소

--- src/feature/editor/lib/sanitize.js
+++ src/feature/editor/lib/sanitize.js
@@ -1,32 +1,32 @@
 import DOMPurify from 'dompurify';
 
-const ALLOWED_IFRAME_SOURCES = [
-  /^https?:\/\/(www\.)?youtube\.com\/embed\//,
-  /^https?:\/\/(www\.)?instagram\.com\/(p|reel)\/[a-zA-Z0-9_-]+\/embed\//,
-  /^https?:\/\/(www\.)?tiktok\.com\/embed\/v2\//,
-  /^https?:\/\/platform\.twitter\.com\/embed\/Tweet\.html\?id=/,
-  /^https?:\/\/(www\.)?tv\.naver\.com\/embed\//,
-  /^https?:\/\/maps\.google\.com\/maps\?.*output=embed/,
-  /^https?:\/\/(www\.)?google\.com\/maps\/embed/,
-  /^https?:\/\/(map\.)?naver\.com\//,
-  /^https?:\/\/naver\.me\//,
-  /^https?:\/\/kko\.to\//
-];
+import { EMBED_SOURCES } from '../constant';
+
+export const isAllowedEmbedUrl = (url) =>
+  typeof url === 'string' &&
+  EMBED_SOURCES.some((source) => source.embedUrlPattern.test(url));
 
 DOMPurify.addHook('afterSanitizeAttributes', (node) => {
   if (node.tagName === 'IFRAME') {
     const src = node.getAttribute('src') || '';
 
-    const isAllowed = ALLOWED_IFRAME_SOURCES.some((pattern) => pattern.test(src));
-      if (!isAllowed) {
-        node.removeAttribute('src');
-      }
+    if (!isAllowedEmbedUrl(src)) {
+      node.removeAttribute('src');
+    }
   }
-})
+});
 
 export const sanitizeHtml = (html) => {
   return DOMPurify.sanitize(html, {
     ADD_TAGS: ['iframe'],
-    ADD_ATTR: ['allow', 'allowfullscreen', 'frameborder', 'scrolling', 'src', 'width', 'height'],
+    ADD_ATTR: [
+      'allow',
+      'allowfullscreen',
+      'frameborder',
+      'scrolling',
+      'src',
+      'width',
+      'height',
+    ],
   });
 };
