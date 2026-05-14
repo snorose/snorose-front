b1e80de8842901fc445dbe32c86fec1b96590f06
haesa <34948133+haesa@users.noreply.github.com>
2026-05-14 20:00:09 +0900

    [SNO-426] 문의 및 신고 작성/수정 완료 확인 모달 표시 (#1634)
    * [FIX] 문의 및 신고 작성/수정 완료 확인 모달 표시
    
    * [REFACTOR] path param - postId 타입안정성 강화
    
    * [REFACTOR] #1633 타입안정성 강화
    
    * [REFACTOR] #1633 타입안정성 강화
    
    * [REFACTOR] #1633 타입안정성 강화
    
    * [REFACTOR] #1633 타입안정성 강화
    
    * [FIX] #1633 신고글 수정 완료 토스트 메시지

--- src/page/support/WriteInquiryPage.tsx
+++ src/page/support/WriteInquiryPage.tsx
@@ -1,27 +1,46 @@
+import { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
-import { BOARD_ID, QUERY_KEY, TOAST } from '@/shared/constant';
+import { ConfirmModal } from '@/shared/component';
+import {
+  BOARD_ID,
+  CONFIRM_MODAL_TEXT,
+  QUERY_KEY,
+  TOAST,
+} from '@/shared/constant';
+import { ModalContext } from '@/shared/context/ModalContext';
 import { useToast } from '@/shared/hook';
 
 import { mapFileToAttachment } from '@/feature/attachment/lib';
+import { Attachment, UploadFile } from '@/feature/attachment/types';
 import { createInquiry } from '@/feature/support/api';
 import { INQUIRY_PLACEHOLDERS } from '@/feature/support/constant';
 import { INQUIRY_OPTIONS } from '@/feature/support/data';
 import { SupportFormView } from '@/feature/support/ui';
 
 import { createThumbnail } from '@/apis';
+import { Option } from '@/types';
 
 export default function WriteInquiryPage() {
   const navigate = useNavigate();
 
   const queryClient = useQueryClient();
 
+  const { modal, setModal } = useContext(ModalContext);
   const { toast } = useToast();
 
   const { mutate: submitInquiry } = useMutation({
-    mutationFn: createInquiry,
+    mutationFn: () =>
+      createInquiry({
+        title,
+        content,
+        inquiryCategory: selectedOption!.key,
+        target: url,
+        attachments: files.map(mapFileToAttachment),
+      }),
+
     onSuccess: (data) => {
       const { postId } = data;
 
@@ -41,28 +60,50 @@ export default function WriteInquiryPage() {
 
       navigate(`/inquiry/${postId}`, { replace: true });
     },
+
     onError: (error) => {
       toast({ message: error.message, variant: 'error' });
     },
   });
 
-  const { dropdown, title, content } = INQUIRY_PLACEHOLDERS;
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [url, setUrl] = useState('');
+  const [selectedOption, setSelectedOption] = useState<Option | undefined>();
+  const [attachments, setAttachments] = useState<Attachment[]>([]);
+  const [files, setFiles] = useState<UploadFile[]>([]);
 
   return (
-    <SupportFormView
-      submit={({ title, content, selectedOption, url, files }) =>
-        submitInquiry({
-          title,
-          content,
-          inquiryCategory: selectedOption.key,
-          target: url,
-          attachments: files.map(mapFileToAttachment),
-        })
-      }
-      options={INQUIRY_OPTIONS}
-      contentLabel={'문의 내용'}
-      placeholders={{ dropdown, title, content }}
-      showLinkField
-    />
+    <>
+      <SupportFormView
+        title={title}
+        content={content}
+        url={url}
+        selectedOption={selectedOption}
+        attachments={attachments}
+        files={files}
+        setTitle={setTitle}
+        setContent={setContent}
+        setUrl={setUrl}
+        setSelectedOption={setSelectedOption}
+        setAttachments={setAttachments}
+        setFiles={setFiles}
+        options={INQUIRY_OPTIONS}
+        contentLabel={'문의 내용'}
+        placeholders={INQUIRY_PLACEHOLDERS}
+        showLinkField
+        modalId='confirm-inquiry-write'
+      />
+
+      {modal.id === 'confirm-inquiry-write' && (
+        <ConfirmModal
+          modalText={CONFIRM_MODAL_TEXT.INQUIRY_CREATE}
+          onConfirm={() => {
+            submitInquiry();
+          }}
+          onCancel={() => setModal({ id: null })}
+        />
+      )}
+    </>
   );
 }
