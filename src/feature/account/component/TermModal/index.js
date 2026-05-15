dc191de6eff53d455f04f271d1e168a7ef214449
하지민 <163178666+JiiminHa@users.noreply.github.com>
2026-05-15 16:14:24 +0900

    [REFACTOR] #1583 약관 모달 구조 단순화

--- src/feature/account/component/TermModal/index.js
+++ src/feature/account/component/TermModal/index.js
@@ -1,3 +1,1 @@
 export { default as PrivacyTermModal } from './PrivacyTermModal';
-export { default as NotiTermModal } from './NotiTermModal';
-export { default as MarketingTermModal } from './MarketingTermModal';
