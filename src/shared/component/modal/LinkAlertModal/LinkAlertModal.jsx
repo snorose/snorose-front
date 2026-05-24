7f6b5b8a4a294764323c225908af36bf424c7dec
gain-0525 <kimgain0525@sookmyung.ac.kr>
2026-05-25 00:09:20 +0900

    [REFACTOR] #1656 LinkAlertModal 모달 재사용

--- src/shared/component/modal/LinkAlertModal/LinkAlertModal.jsx
+++ src/shared/component/modal/LinkAlertModal/LinkAlertModal.jsx
@@ -1,4 +1,4 @@
-import { Icon } from '@/shared/component';
+import { ConfirmModal, Icon } from '@/shared/component';
 
 import styles from './LinkAlertModal.module.css';
 
@@ -11,67 +11,38 @@ export default function LinkAlertModal({
 }) {
   if (!isOpen) return null;
 
-  const handleConfirm = () => {
-    if (checked) {
-      localStorage.setItem('hideLinkAlert', 'true');
-    }
-
-    onConfirm();
-  };
-  
   return (
-    <div className={styles.dim} onClick={onClose}>
+    <ConfirmModal
+      modalText={{
+        title: '외부 링크로 연결할까요?',
+        description:
+          '다른 사용자가 공유한 외부 링크의 <br />안전 여부는 스노로즈에서 내용을 <br />확인하기 어려우니 주의해주세요',
+        cancelText: '취소',
+        confirmText: '연결',
+      }}
+      onCancel={onClose}
+      onConfirm={onConfirm}
+      confirmButtonClassName={styles.linkConfirmButton}
+    >
       <div
-        className={styles.container}
-        onClick={(e) => e.stopPropagation()}
+        className={styles.checkRow}
+        onClick={() => setChecked(!checked)}
       >
-        <div className={styles.content}>
-          <h2 className={styles.title}>
-            이 링크에 연결할까요?
-          </h2>
-
-          <p className={styles.description}>
-            신뢰할 수 있는 링크인지
-            <br />
-            한 번 더 확인해주세요
-          </p>
-
-          <div
-            className={styles.checkRow}
-            onClick={() => setChecked(!checked)}
-          >
-            <div
-              className={`${styles.checkCircle} ${checked ? styles.checked: ''}`}
-            >
-              <Icon
-                id='check-editor'
-                className={styles.checkIcon}
-              />
-
-            </div>
-
-            <span className={styles.checkText}>
-              이 게시판에서 다시 보지 않기
-            </span>
-          </div>
+        <div
+          className={`${styles.checkCircle} ${
+            checked ? styles.checked : ''
+          }`}
+        >
+          <Icon
+            id='check-editor'
+            className={styles.checkIcon}
+          />
         </div>
 
-        <div className={styles.bottom}>
-          <button
-            className={`${styles.button} ${styles.cancelBtn}`}
-            onClick={onClose}
-          >
-            취소
-          </button>
-
-          <button
-            className={`${styles.button} ${styles.confirmBtn}`}
-            onClick={handleConfirm}
-          >
-            연결
-          </button>
-        </div>
+        <span className={styles.checkText}>
+          이 게시판에서 다시 보지 않기
+        </span>
       </div>
-    </div>
+    </ConfirmModal>
   );
 }
