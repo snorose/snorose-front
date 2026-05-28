c79666b8f42df4504fcd35bfa99228ba17682be7
haesa <xdhaesa@gmail.com>
2026-05-28 20:19:45 +0900

    [REFACTOR] #1653 Http Status Code 추가 및 인프라 에러 상태 코드 타입 수정

--- src/shared/constant/http.ts
+++ src/shared/constant/http.ts
@@ -1,10 +1,14 @@
 import { invertObject } from '@/shared/lib/object';
 
-export const HTTP_STATUS = {
+export const HTTP_STATUS_CODE = {
   BAD_REQUEST: 400,
   UNAUTHORIZED: 401,
   FORBIDDEN: 403,
   NOT_FOUND: 404,
+  CONFLICT: 409,
+  PAYLOAD_TOO_LARGE: 413,
+  UNPROCESSABLE_ENTITY: 422,
+  TOO_MANY_REQUESTS: 429,
 
   INTERNAL_SERVER_ERROR: 500,
   BAD_GATEWAY: 502,
@@ -12,13 +16,18 @@ export const HTTP_STATUS = {
   GATEWAY_TIMEOUT: 504,
 } as const;
 
-export const HTTP_STATUS_NAME = invertObject(HTTP_STATUS);
+export const HTTP_STATUS_NAME = invertObject(HTTP_STATUS_CODE);
 
-type HttpStatusCode = (typeof HTTP_STATUS)[keyof typeof HTTP_STATUS];
+export const INFRA_ERROR_STATUS_CODE = [
+  HTTP_STATUS_CODE.BAD_GATEWAY,
+  HTTP_STATUS_CODE.SERVICE_UNAVAILABLE,
+  HTTP_STATUS_CODE.GATEWAY_TIMEOUT,
+] as const;
 
-export type InfraErrorStatusCode =
-  | typeof HTTP_STATUS.BAD_GATEWAY
-  | typeof HTTP_STATUS.SERVICE_UNAVAILABLE
-  | typeof HTTP_STATUS.GATEWAY_TIMEOUT;
+type HttpStatusCode = (typeof HTTP_STATUS_CODE)[keyof typeof HTTP_STATUS_CODE];
 
-export type ApiErrorStatusCode = Exclude<HttpStatusCode, InfraErrorStatusCode>;
+type StrictInfraErrorCode = (typeof INFRA_ERROR_STATUS_CODE)[number];
+
+export type InfraErrorStatusCode = StrictInfraErrorCode | (number & {});
+
+export type ApiErrorStatusCode = Exclude<HttpStatusCode, StrictInfraErrorCode>;
