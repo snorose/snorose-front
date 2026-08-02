f5ce8b16071c82bd83695baa4faa48d589f300d8
dawncoding <sunnuri99@gmail.com>
2026-08-02 21:05:41 +0900

    refactor: 팁탭 내장 ListItem의 Tab/Shift-Tab 동작과 중복되는 부분 제거

--- src/feature/editor/component/extensions/list-tab-handler-extension.js
+++ src/feature/editor/component/extensions/list-tab-handler-extension.js
@@ -1,48 +1,18 @@
 import { Extension } from '@tiptap/core';
-import { Plugin, PluginKey } from 'prosemirror-state';
-
-const isInList = (state) => {
-  const { $from } = state.selection;
-  for (let depth = $from.depth; depth > 0; depth--) {
-    const name = $from.node(depth).type.name;
-    if (name === 'bulletList' || name === 'orderedList') {
-      return true;
-    }
-  }
-  return false;
-};
 
 export const ListTabHandler = Extension.create({
   name: 'listTabHandler',
 
   priority: 1000,
 
-  addProseMirrorPlugins() {
+  addKeyboardShortcuts() {
     const { editor } = this;
 
-    return [
-      new Plugin({
-        key: new PluginKey('listTabHandler'),
-        props: {
-          handleKeyDown: (view, event) => {
-            if (event.key !== 'Tab') {
-              return false;
-            }
-            if (!isInList(view.state)) {
-              return false;
-            }
-
-            if (event.shiftKey) {
-              editor.commands.liftListItem('listItem');
-            } else {
-              editor.commands.sinkListItem('listItem');
-            }
-
-            event.preventDefault();
-            return true;
-          },
-        },
-      }),
-    ];
+    return {
+      Tab: () =>
+        editor.commands.sinkListItem('listItem') || editor.isActive('listItem'),
+      'Shift-Tab': () =>
+        editor.commands.liftListItem('listItem') || editor.isActive('listItem'),
+    };
   },
 });
