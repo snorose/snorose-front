69d80c0395e55f390f7dcee537bb393e94d420b0
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2026-08-07 11:37:17 +0900

    refactor: #1718 게시판 카테고리 하드코딩 제거 후 상수화

--- src/page/board/BoardCategoryPage/BoardCategoryPage.jsx
+++ src/page/board/BoardCategoryPage/BoardCategoryPage.jsx
@@ -1,21 +1,22 @@
 import { useState } from 'react';
 
 import { Header } from '@/shared/component';
-import { BOARD_MENUS } from '@/shared/constant';
+import { BOARD_CATEGORY, BOARD_MENUS } from '@/shared/constant';
 
 import { BoardBar, AccordianBoards } from '@/feature/board/component';
 import { Search } from '@/feature/search/component';
 
 import styles from './BoardCategoryPage.module.css';
 
+const { HIDDEN, ...VISIBLE_BOARD_CATEGORY } = BOARD_CATEGORY;
 const FAVORITE_BOARD_IDS = [20, 61]; //즐겨찾기 더미 데이터
 
 export default function BoardCategoryPage() {
-  const [openBoards, setOpenBoards] = useState({
-    community: true,
-    official: true,
-    snorose: true,
-  });
+  const [openBoards, setOpenBoards] = useState(
+    Object.fromEntries(
+      Object.values(VISIBLE_BOARD_CATEGORY).map(({ value }) => [value, true])
+    )
+  );
   const toggleBoard = (boardName) => {
     setOpenBoards((prev) => ({
       ...prev,
@@ -31,59 +32,27 @@ export default function BoardCategoryPage() {
       </div>
 
       <div className={styles.paddingContainer}>
-        <div className={styles.boardBox}>
-          <AccordianBoards
-            title='커뮤니티'
-            isOpen={openBoards.community}
-            onClick={() => toggleBoard('community')}
-          >
-            <div className={styles.boardListBox}>
-              {BOARD_MENUS.filter((board) =>
-                [20, 21, 22, 23].includes(board.id)
-              ).map((board, index) => (
-                <BoardBar
-                  key={board.id}
-                  data={board}
-                  isFavorite={FAVORITE_BOARD_IDS.includes(board.id)}
-                />
-              ))}
-            </div>
-          </AccordianBoards>
-        </div>
-        <div className={styles.boardBox}>
-          <AccordianBoards
-            title='공식 게시판'
-            isOpen={openBoards.official}
-            onClick={() => toggleBoard('official')}
-          >
-            <div className={styles.boardListBox}>
-              {BOARD_MENUS.filter((board) =>
-                [60, 61, 62].includes(board.id)
-              ).map((board, index) => (
-                <BoardBar
-                  key={board.id}
-                  data={board}
-                  isFavorite={FAVORITE_BOARD_IDS.includes(board.id)}
-                />
-              ))}
-            </div>
-          </AccordianBoards>
-        </div>
-        <div className={styles.boardBox}>
-          <AccordianBoards
-            title='스노로즈 게시판'
-            isOpen={openBoards.snorose}
-            onClick={() => toggleBoard('snorose')}
-          >
-            <div className={styles.boardListBox}>
-              {BOARD_MENUS.filter((board) =>
-                ['event'].includes(board.textId)
-              ).map((board, index) => (
-                <BoardBar key={board.id} data={board} />
-              ))}
-            </div>
-          </AccordianBoards>
-        </div>
+        {Object.values(VISIBLE_BOARD_CATEGORY).map((category, index) => (
+          <div className={styles.boardBox}>
+            <AccordianBoards
+              title={category.label}
+              isOpen={openBoards[category.value]}
+              onClick={() => toggleBoard(category.value)}
+            >
+              <div className={styles.boardListBox}>
+                {BOARD_MENUS.filter((board) => board.category === category).map(
+                  (board, index) => (
+                    <BoardBar
+                      key={board.id}
+                      data={board}
+                      isFavorite={FAVORITE_BOARD_IDS.includes(board.id)}
+                    />
+                  )
+                )}
+              </div>
+            </AccordianBoards>
+          </div>
+        ))}
       </div>
     </div>
   );
