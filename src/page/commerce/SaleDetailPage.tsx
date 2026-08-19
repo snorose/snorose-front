bed843eb59556e6bd50a66f0ba245922ab95cf96
haesa <xdhaesa@gmail.com>
2026-08-20 03:06:04 +0900

    feat: 판매 상세 상품 이미지 캐러셀 추가

--- src/page/commerce/SaleDetailPage.tsx
+++ src/page/commerce/SaleDetailPage.tsx
@@ -1,5 +1,9 @@
-import { BackAppBar } from '@/shared/component';
+import { useState } from 'react';
+
+import { BackAppBar, Carousel } from '@/shared/component';
 import { DateTime } from '@/shared/lib';
+
+import ProductCard from '@/page/commerce/ProductCard';
 
 import { sale } from '@/dummy/data/sale';
 
@@ -18,6 +22,42 @@ export default function SaleDetailPage() {
           {sale.pickup.instructions}
         </span>
       </section>
+
+      <ProductSection />
+
+      <div className={styles.border} />
     </div>
+  );
+}
+
+function ProductSection() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const carouselItems = sale.products.flatMap((product) =>
+    product.imageUrls.map((imageUrl) => ({
+      ...product,
+      imageUrls: undefined,
+      imageUrl,
+    }))
+  );
+
+  const activeItem = carouselItems[activeIndex];
+
+  return (
+    <section>
+      <Carousel
+        className={styles.productCarousel}
+        items={carouselItems}
+        renderItem={(item) => (
+          <ProductCard src={item.imageUrl} alt={item.name ?? sale.title} />
+        )}
+        onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)}
+        autoplay={false}
+      />
+      <div className={styles.productInfo}>
+        <h2 className={styles.productName}>{activeItem.name}</h2>
+        <p className={styles.productDescription}>{activeItem.description}</p>
+      </div>
+    </section>
   );
 }
