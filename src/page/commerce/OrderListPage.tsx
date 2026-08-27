6e004810621f97a328a9671588666caf5110bb6a
haesa <xdhaesa@gmail.com>
2026-08-28 01:21:36 +0900

    feat: 주문 목록 UI

--- src/page/commerce/OrderListPage.tsx
+++ src/page/commerce/OrderListPage.tsx
@@ -1,11 +1,14 @@
 import { Suspense } from 'react';
+import { Link } from 'react-router-dom';
 
 import { ErrorBoundary } from '@sentry/react';
 
 import { BackAppBar } from '@/shared/component';
 import { flatPaginationCache } from '@/shared/lib';
 
+import OrderItem from '@/feature/commerce/components/OrderItem';
 import useOrders from '@/feature/commerce/hooks/useOrders';
+import type { OrdersResponse } from '@/feature/commerce/types';
 
 import { noPostsIllustration } from '@/assets/illustrations';
 
@@ -23,7 +26,35 @@ export default function OrderListPage() {
 
 function OrderListView() {
   const { data } = useOrders();
-  const orders = flatPaginationCache(data);
+  const orders: OrdersResponse['data'] = flatPaginationCache(data);
 
-  return <div>내 주문 목록</div>;
+  if (orders.length === 0) {
+    return (
+      <div className={styles.noContentWrapper}>
+        <p className={styles.noContentMessage}>주문 내역이 없어요</p>
+        <div className={styles.imageWrapper}>
+          <img
+            src={noPostsIllustration}
+            width={220}
+            height={182}
+            alt='주문 목록이 없음'
+          />
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles.container}>
+      <BackAppBar title='내 주문 목록' />
+
+      <div className={styles.orderList}>
+        {orders.map((order) => (
+          <Link key={order.orderNumber} to={order.orderNumber}>
+            <OrderItem {...order} />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 }
--- src/shared/component/Chip/Chip.tsx
+++ src/shared/component/Chip/Chip.tsx
@@ -3,8 +3,13 @@ import styles from './Chip.module.css';
 type ChipPropsType = {
   name: string;
   variant: 'grey' | 'gradient';
+  size?: 'md' | 'sm';
 };
 
-export default function Chip({ name, variant }: ChipPropsType) {
-  return <div className={`${styles.container} ${styles[variant]}`}>{name}</div>;
+export default function Chip({ name, variant, size = 'md' }: ChipPropsType) {
+  return (
+    <div className={`${styles.container} ${styles[variant]} ${styles[size]}`}>
+      {name}
+    </div>
+  );
 }
