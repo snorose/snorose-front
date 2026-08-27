7dc602ea5f73176ea8aa9ddbad68e6aebc0256d7
haesa <xdhaesa@gmail.com>
2026-08-28 05:39:50 +0900

    feat: 주문 취소 시 주문 목록 캐시 무효화

--- src/feature/commerce/hooks/useOrderCancel.ts
+++ src/feature/commerce/hooks/useOrderCancel.ts
@@ -1,11 +1,17 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { QUERY_KEY } from '@/shared/constant';
 
 import { cancelOrder } from '@/feature/commerce/apis';
 
 export default function useOrderCancel() {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: cancelOrder,
-    onSuccess: () => {},
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: QUERY_KEY.commerceOrders });
+    },
     onError: () => {},
   });
 }
