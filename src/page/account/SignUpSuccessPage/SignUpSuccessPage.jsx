272cea331e68335ef71963f697b9e0de71d2db34
misung.dev <misung.dev@gmail.com>
2026-09-06 17:39:19 +0900

    style: 회원가입 완료 페이지 체크 아이콘 JSX 정렬

--- src/page/account/SignUpSuccessPage/SignUpSuccessPage.jsx
+++ src/page/account/SignUpSuccessPage/SignUpSuccessPage.jsx
@@ -12,11 +12,7 @@ export default function SignUpSuccessPage() {
   return (
     <div className={styles.container}>
       <div>
-        <IconCheck
-          width={'2.4rem'}
-          height={'2.4rem'}
-          className={styles.icon}
-        />
+        <IconCheck width={'2.4rem'} height={'2.4rem'} className={styles.icon} />
         <p className={styles.title}>스노로즈 가입이 완료되었어요!</p>
         <p className={styles.explanation}>
           별도의 인증 절차 후<br />
