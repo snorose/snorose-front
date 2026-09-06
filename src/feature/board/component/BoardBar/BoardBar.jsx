b95805981c1b75dd154766b2487988a25d76d298
하지민 <163178666+JiiminHa@users.noreply.github.com>
2026-09-06 14:28:56 +0900

    refactor: BoardBar 일러스트 컴포넌트 지원

--- src/feature/board/component/BoardBar/BoardBar.jsx
+++ src/feature/board/component/BoardBar/BoardBar.jsx
@@ -9,9 +9,15 @@ export default function BoardBar({
   isFavorite = false,
   onFavoriteClick = () => {},
 }) {
+  const Image = data.image;
+
   return (
     <Link to={`/board/${data.textId}`} className={styles.container}>
-      <img className={styles.image} src={data.image} alt={data.textId} />
+      {typeof Image === 'string' ? (
+        <img className={styles.image} src={Image} alt={data.textId} />
+      ) : (
+        <Image className={styles.image} aria-label={data.textId} />
+      )}
       <div className={styles.textBox}>
         <h3 className={styles.title}>{data.title}</h3>
         <p className={styles.description}>{data.desc}</p>
@@ -37,9 +43,15 @@ export default function BoardBar({
  * TODO(board): 라우트 개선 작업 완료 후 교체
  */
 export function NewBoardBar({ name, to, desc, image }) {
+  const Image = image;
+
   return (
     <Link to={to} className={styles.container}>
-      <img className={styles.image} src={image} alt={name} />
+      {typeof Image === 'string' ? (
+        <img className={styles.image} src={Image} alt={name} />
+      ) : (
+        <Image className={styles.image} aria-label={name} />
+      )}
       <div className={styles.textBox}>
         <h3 className={styles.title}>{name}</h3>
         <p className={styles.description}>{desc}</p>
