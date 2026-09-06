d13d8ab25e4c62ceda7885bb4158aa53e4415e8b
하지민 <163178666+JiiminHa@users.noreply.github.com>
2026-09-06 12:11:05 +0900

    refactor: IconSearch 컴포넌트로 대체

--- src/shared/component/layout/BackAppBar/BackAppBar.tsx
+++ src/shared/component/layout/BackAppBar/BackAppBar.tsx
@@ -1,5 +1,7 @@
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 
+import { IconSearch } from '@snorose/icons';
+
 import { Icon, MenuIcon } from '@/shared/component';
 
 import styles from './BackAppBar.module.css';
@@ -50,7 +52,7 @@ export default function BackAppBar({
       <div className={hasSearchInput ? styles.hasWideWidth : styles.actions}>
         {hasSearch && (
           <Link to={`${pathname}/search`}>
-            <Icon id='search-thick' width={20} height={20} stroke='#00368E' />
+            <IconSearch width={20} height={20} color='var(--blue-4)' />
           </Link>
         )}
         {hasMenu && <MenuIcon />}
