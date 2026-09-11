302f2c25b8d29bebea30237fece666a2477dd534
haesa <xdhaesa@gmail.com>
2026-09-11 15:44:46 +0900

    chore: vite-plugin-svgr 의존성 추가

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   build: {
     outDir: 'build',
   },
-  plugins: [react()],
+  plugins: [react(), svgr({ svgrOptions: { icon: true } })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
