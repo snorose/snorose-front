a3ac1aa91001aa3cd1a1390608569d17d527b02e
haesa <xdhaesa@gmail.com>
2026-09-12 00:13:41 +0900

    fix: IconMultiCheckGreyCircle 중복 import 해결

--- src/page/account/LoginPage/LoginPage.jsx
+++ src/page/account/LoginPage/LoginPage.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
-import { IconMultiCheckGreyCircle } from '@snorose/icons';
-
 import {
   IconMultiCheckBlueCircle,
   IconMultiCheckGreyCircle,
