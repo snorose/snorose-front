9be08c39595c8df6939af311d82cb435e602c75c
하지민 <163178666+JiiminHa@users.noreply.github.com>
2026-09-11 22:32:04 +0900

    refactor: IconMultiExclamationTriangle과 IconMultiInfoCircle 컴포넌트로 교체

--- src/shared/component/Toast/Toast.jsx
+++ src/shared/component/Toast/Toast.jsx
@@ -1,6 +1,11 @@
 import { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
+import {
+  IconMultiExclamationTriangle,
+  IconMultiInfoCircle,
+} from '@snorose/icons';
+
 import { Icon } from '@/shared/component';
 import { useToastContext } from '@/shared/context/ToastContext';
 
@@ -52,7 +57,17 @@ export default function Toast({ toast }) {
 
   return createPortal(
     <div ref={toastRef} className={toastClassName}>
-      <Icon className={styles.icon} id={iconId} width={21} height={20} />
+      {iconId === 'info-triangle' ? (
+        <IconMultiExclamationTriangle
+          className={styles.icon}
+          width={21}
+          height={20}
+        />
+      ) : iconId === 'info-circle' ? (
+        <IconMultiInfoCircle className={styles.icon} width={21} height={20} />
+      ) : (
+        <Icon className={styles.icon} id={iconId} width={21} height={20} />
+      )}
       <p className={styles.message}>{toast.message}</p>
     </div>,
     root
