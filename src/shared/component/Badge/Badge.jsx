accfa683b6851db72b6bc78617ec54c7f320764e
becherryu <81617058+becherryu@users.noreply.github.com>
2026-09-12 01:13:33 +0900

    fix: 목록 카드 아이콘 사이즈 수정

--- src/shared/component/Badge/Badge.jsx
+++ src/shared/component/Badge/Badge.jsx
@@ -2,13 +2,25 @@ import { IconMultiBadgeBlue, IconMultiBadgePink } from '@snorose/icons';
 
 import { ROLE } from '@/shared/constant';
 
-export default function Badge({ userRoleId, className = '' }) {
+export default function Badge({ userRoleId, className = '', ...iconProps }) {
   if (userRoleId === ROLE.admin) {
-    return <IconMultiBadgeBlue aria-label='리자 뱃지' className={className} />;
+    return (
+      <IconMultiBadgeBlue
+        {...iconProps}
+        aria-label='리자 뱃지'
+        className={className}
+      />
+    );
   }
 
   if (userRoleId === ROLE.official) {
-    return <IconMultiBadgePink aria-label='공식 뱃지' className={className} />;
+    return (
+      <IconMultiBadgePink
+        {...iconProps}
+        aria-label='공식 뱃지'
+        className={className}
+      />
+    );
   }
 
   return null;
