cc18be70348f9ffc888c65b285b9daf3a17df5ac
misung-dev <misung.dev@gmail.com>
2026-09-21 01:31:40 +0900

    chore: CSS 저장 시 자동 정렬 및 속성 간 공백 제거 설정 추가

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,5 +1,16 @@
+import propertyGroups from 'stylelint-config-recess-order/groups';
+
 const stylelintConfig = {
-  extends: ['stylelint-config-recess-order'],
+  plugins: ['stylelint-order'],
+  rules: {
+    'comment-empty-line-before': 'never',
+    'declaration-empty-line-before': 'never',
+    'order/properties-order': propertyGroups.map((group) => ({
+      ...group,
+      emptyLineBefore: 'never',
+      noEmptyLineBetween: true,
+    })),
+  },
 };
 
 export default stylelintConfig;
