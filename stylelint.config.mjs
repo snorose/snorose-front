6f5c44eae45b1480ab401ac65091c39cb37b75e1
misung-dev <misung.dev@gmail.com>
2026-09-21 01:37:06 +0900

    chore: CSS 속성 8개 이상일 때 그룹 간 공백 적용

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -3,13 +3,16 @@ import propertyGroups from 'stylelint-config-recess-order/groups';
 const stylelintConfig = {
   plugins: ['stylelint-order'],
   rules: {
-    'comment-empty-line-before': 'never',
-    'declaration-empty-line-before': 'never',
-    'order/properties-order': propertyGroups.map((group) => ({
-      ...group,
-      emptyLineBefore: 'never',
-      noEmptyLineBetween: true,
-    })),
+    'order/properties-order': [
+      propertyGroups.map((group) => ({
+        ...group,
+        emptyLineBefore: 'threshold',
+        noEmptyLineBetween: true,
+      })),
+      {
+        emptyLineMinimumPropertyThreshold: 8,
+      },
+    ],
   },
 };
 
